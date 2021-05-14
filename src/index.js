3d38e20512ff326a8774cedab3e6cd145bdd4fe9
Irfan Muhammad Abyan <irfanabyan96@gmail.com>
2021-05-14 08:07:07 +0700

    fix: Fixed build configs.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ require('dotenv').config();
 const { Client, MessageEmbed } = require('discord.js');
 // eslint-disable-next-line import/order
 const util = require('./utils');
+
 const client = new Client({});
 const { promisify } = require('util');
 const { resolve } = require('path');
 const readdir = promisify(require('fs').readdir);
+
+const express = require('express');
+
+const app = express();
 
 async function* getFiles(dir) {
   const dirents = await readdir(dir, { withFileTypes: true });
@@ -20,6 +25,12 @@ async function* getFiles(dir) {
     }
   }
 }
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+app.listen(8080);
 
 client.commands = new Map();
 
@@ -55,33 +66,32 @@ client.on('message', async (message) => {
   cmd.run(client, message, args);
 });
 
-const gatewayChannelId = '839417251470901279'
-const rulesChannelId = `838751745815216129`
-const luxCastaId = `839210689917616218`
+const gatewayChannelId = '839417251470901279';
+const rulesChannelId = '838751745815216129';
+const luxCastaId = '839210689917616218';
 
 client.on('guildMemberAdd', async (member) => {
-  console.log(member)
+  console.log(member);
 
-  const role = member.guild.roles.cache.get(luxCastaId)
-  await member.roles.add(role.id).catch(err => console.log(err))
+  const role = member.guild.roles.cache.get(luxCastaId);
+  await member.roles.add(role.id).catch((err) => console.log(err));
 
-  const channel = member.guild.channels.cache.get(gatewayChannelId)
-       
+  const channel = member.guild.channels.cache.get(gatewayChannelId);
+
   const embed = new MessageEmbed()
     .setTitle(`Welcome to ${member.guild.name}`)
-    .setThumbnail(member.user.displayAvatarURL({dynamic: true, size: 512}))
+    .setThumbnail(member.user.displayAvatarURL({ dynamic: true, size: 512 }))
     .setDescription(`<@${member.user.id}> please understand our **wisdom of lleud** at ${member.guild.channels.cache.get(rulesChannelId).toString()} as you make your way through this warm sanctuary`)
-    .setFooter(`Fellow seeker ${member.user.username}#${member.user.discriminator}`,member.user.displayAvatarURL({dynamic: true, size: 512}))
-    .setColor('7289da')
-  channel.send(embed)
-})
+    .setFooter(`Fellow seeker ${member.user.username}#${member.user.discriminator}`, member.user.displayAvatarURL({ dynamic: true, size: 512 }))
+    .setColor('7289da');
+  channel.send(embed);
+});
 
 client.on('guildMemberRemove', async (member) => {
-  
-  const channel = member.guild.channels.cache.get(gatewayChannelId)
-  
+  const channel = member.guild.channels.cache.get(gatewayChannelId);
+
   const embed = new MessageEmbed()
-    .setDescription(`The lights get dimmed! <@${member.user.id}> leaving the sanctuary`,member.user.displayAvatarURL({dynamic: true, size: 512}))
-    .setColor('RED')
-  channel.send(embed)
-})
+    .setDescription(`The lights get dimmed! <@${member.user.id}> leaving the sanctuary`, member.user.displayAvatarURL({ dynamic: true, size: 512 }))
+    .setColor('RED');
+  channel.send(embed);
+});
