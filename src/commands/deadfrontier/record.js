5147ba05b0cc073fb34e8130f209d73aa55be852
imsoondae <im.daeva@gmail.com>
2021-11-09 21:21:35 +0700

    new Config update

--- src/commands/deadfrontier/record.js
+++ src/commands/deadfrontier/record.js
@@ -38,7 +38,7 @@ module.exports.run = async (client, message, args) => {
             .addField(`**Daily TPK**`, daily_tpk, true)
             .addField(`**Weekly TPK**`, weekly_tpk, true)
             .addField(`**⭐ All Time TPK**`, all_time_tpk, true)
-            .setImage(`https://www.dfprofiler.com/signaturereplicate.php?profile=${survivorID}&imgur=5q7hV6B.png`)
+            .setImage(`https://www.dfprofiler.com/signaturereplicate.php?profile=${survivorID}&imgur=5q7hV6B`)
             .setFooter(`Powered by Ancient Luna`)
             .setTimestamp()
 
