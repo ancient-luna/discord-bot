d0f715aaa4f42c7ce2319215d957aaf20a734434
imsoondae <im.daeva@gmail.com>
2021-12-12 18:20:21 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -50,7 +50,7 @@ client.on('ready', async () => {
   rulesChannelId = gConfig.server.ruleChannel;
   luxCastaId = gConfig.server.onJoinConfig.preMemberRole;
 
-  client.user.setActivity('snows falling from cabin', {
+  client.user.setActivity('snows falling', {
     type: 'WATCHING',
   });
 
