2e17b5b47603efc4006cfd1f4362fbf6578f4f08
imsoondae <im.daeva@gmail.com>
2022-03-13 01:13:53 +0700

    updated toram world

--- src/index.js
+++ src/index.js
@@ -50,7 +50,7 @@ client.on('ready', async () => {
   rulesChannelId = gConfig.server.ruleChannel;
   luxCastaId = gConfig.server.onJoinConfig.preMemberRole;
 
-  client.user.setActivity('us over the moon', {
+  client.user.setActivity('us from Toram World', {
     type: 'WATCHING',
   });
 
