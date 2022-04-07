e3d27ad425351480de84877634c1a0d6b105344d
imsoondae <im.daeva@gmail.com>
2022-04-07 19:37:23 +0700

    add Feat; Alliance

--- src/commands/toramonline/alliance.js
+++ src/commands/toramonline/alliance.js
@@ -11,15 +11,15 @@ module.exports.run = async (Client, message, args) => {
 
     if (!role) return message.reply('please mention a role after user')
 
-    message.guild.channels.cache.get('860531645916774401').send(`<@${target.user.id}> joining us as alliance from ${role}. Hope we can through this long journey together around Inner City`).then(target.roles.add(role));
+    message.guild.channels.cache.get('952164768217706496').send(`<@${target.user.id}> joining us as alliance from ${role}. Hope we can through this long journey together around Toram world`).then(target.roles.add(role));
     
     await message.delete();
 
     const addDF = new MessageEmbed()
-        .setAuthor({ name: "ROLE ADDED" }, { name: "https://i.imgur.com/aLkmV4I.png" })
-        .setDescription(`You have been gived **Alliances** role and have access to several channels in **Dead Frontier** category. Hope we can through this long journey together around Inner City`)
+        .setAuthor({ name: "ROLE ADDED" }, { name: "https://i.imgur.com/ejkkWCB.png" })
+        .setDescription(`You have been gived **Alliances** role and have access to several channels in **[Departure from Iruna](https://discord.gg/hgjY95ZDQg)** category. Hope we can through this long journey together around Toram world`)
         .setTimestamp()
-        .setFooter({ text: "#Ancient Luna" })
+        .setFooter({ text: "Ancient Luna Guild: We ran as if to meet the moon" })
     await target.user.send({ embeds: [addDF] })
 }
 
