f98dd7ef75b8cd89f637edb9c3e0a53d19ff8908
imsoondae <im.daeva@gmail.com>
2022-05-08 23:43:53 +0700

    add: New Feat

--- src/commands/toramonline/map.js
+++ src/commands/toramonline/map.js
@@ -1,37 +1,20 @@
-const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 
 module.exports.run = async (Client, message, args) => {
-    const ar = "<a:_util_arrow:864810269771300875>"
 
-    let map = new MessageEmbed()
+    const mapIruna = new MessageEmbed()
         .setTitle("World Map")
         .setURL("https://discord.com/invite/Sbp2nt8QHe")
-        .setDescription(`Iruna World is a world that created by combining places brought together by the 12 gods into one. After collapsing due to Cataclysm, it was performed randomly while Toram World is a different world that caused a Cataclysm after Coenubias made it collide with Iruna. It largely remains a mystery.`)
-        .setImage("https://i.imgur.com/dpYHOPq.png")
+        .setImage("https://i.imgur.com/UIxyMwM.jpg")
+        .setColor("#2f3136")
+
+    const mapToram = new MessageEmbed()
+        .setImage("https://i.imgur.com/dam0jwe.jpg")
         .setTimestamp()
         .setFooter({ text: "Powered by Ancient Luna", iconURL: 'https://i.imgur.com/QZ2gLgq.png' })
-        .setColor("#4f545c")
-        
-    let iruna = new MessageActionRow()
-        .addComponents(
-            new MessageButton()
-                .setStyle('LINK')
-                .setLabel('Iruna World Map')
-                .setURL('https://toram-id.info/dye')
-        )
-        
-    let toram = new MessageActionRow()
-        .addComponents(
-            new MessageButton()
-                .setStyle('LINK')
-                .setLabel('Toram World Map')
-                .setURL('https://toram-id.info/dye')
-        )
-
-    await message.reply({
-        embeds: [dye],
-        components: [iruna, toram]
-    }).catch(e => {});
+        .setColor("#2f3136")
+    
+    message.reply({ embeds: [mapIruna, mapToram] }).catch(e => {});
 }
 
 module.exports.help = {
