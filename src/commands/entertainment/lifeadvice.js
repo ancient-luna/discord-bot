5268de029cd90d769dca95bc462b5b479f589dfd
imsoondae <im.daeva@gmail.com>
2022-05-25 03:56:16 +0700

    add random advice

--- src/commands/entertainment/lifeadvice.js
+++ src/commands/entertainment/lifeadvice.js
@@ -1,12 +1,19 @@
 const { MessageAttachment } = require('discord.js');
+const axios =  require('axios');
 
 module.exports.run = async (client, message, args) => {
 
+    const response = await axios.get('https://api.adviceslip.com/advice');
+
     let IMGlife = new MessageAttachment("src/assets/dduckdaegood.png")
 
-    await message.channel.send({ files: [IMGlife] }).catch((e) => {});
+    await message.channel.send({
+        content: `${response.data.slip.advice}\n⁣`,
+        files: [IMGlife]
+    }).catch((e) => {});
 }
 
 module.exports.help = {
-    name: 'lifeadvice'
+    name: 'lifeadvice',
+    aliases: ['advice']
 }
