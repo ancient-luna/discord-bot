0de5f85af7d06edd4088ddcd7d6a3de22332f68c
imsoondae <im.daeva@gmail.com>
2022-05-25 03:57:12 +0700

    fix configs

--- src/commands/entertainment/youtube.js
+++ src/commands/entertainment/youtube.js
@@ -1,53 +1,36 @@
 const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
-const fetch = require("node-fetch");
+const discordTogether = require("../../utils/activity")
 
-module.exports.run = async (Client, message) => {
+module.exports.run = async (Client, message, args) => {
     let channel = message.member.voice.channel;
 
     if(!channel) return message.reply(`You have to be in any **voice channel** first in this server`).catch((e) => {});
 
-    fetch (`https://discord.com/api/v8/channels/${channel.id}/invites`, {
-        method: "POST",
-        body: JSON.stringify({
-            max_age: 86400,
-            max_uses: 0,
-            target_application_id: "839380589508558910",
-            target_type: 2,
-            temporary: false,
-            validate: null
-        }),
-        headers: {
-            "Authorization": `Bot ${Client.token}`,
-            "Content-Type": "application/json"
-        }
-    })
-
-    .then(res => res.json())
-    .then(invite => {
-        if(!invite.code) return message.reply("Something wrong with YT-Together link. Don't blame me").catch((e) => {});
-
-        const embed = new MessageEmbed()
-            .setTitle(`Get your snacks and relax 🍿`)
-            .setDescription(`**${message.author.username}** has started **[YouTube Together](https://discord.com/invite/${invite.code})** in **${channel.name}**\nWatch YouTube videos without ads in Ancient Luna`)
-            .setFooter({ text: `Mobile ver. not supported`, iconURL: 'https://i.imgur.com/7WAJS44.png' })
-            .setColor('ed0000')
-
+    discordTogether
+        .createTogetherCode(channel.id, 'youtube')
+        .then((invite) => 
         
-        const buttonWatch = new MessageActionRow()
-            .addComponents(
-                new MessageButton()
-                    .setStyle("LINK")
-                    .setLabel("Join and start watch together")
-                    .setURL(`https://discord.com/invite/${invite.code}`)
+            message.reply({
+                embeds: [
+                    new MessageEmbed()
+                        .setTitle(`Get your snacks and relax 🍿`)
+                        .setDescription(`**${message.author.username}** has started **[YouTube Together](${invite.code})** in <#${channel.id}>\nWatch YouTube videos without ads in Ancient Luna`)
+                        .setFooter({ text: `Mobile ver. not supported`, iconURL: 'https://i.imgur.com/7WAJS44.png' })
+                        .setColor('ed0000')
+                ],
+                components: [
+                    new MessageActionRow()
+                        .addComponents(
+                            new MessageButton()
+                                .setStyle("LINK")
+                                .setLabel("Join and start watch together")
+                                .setURL(invite.code)
+                        )
+                ]
+            }).catch((e) => {})
         )
-        
-        message.reply({
-            embeds: [embed],
-            components: [buttonWatch]
-        }).catch((e) => {});
-    }).catch((e) => {});
 }
 
 module.exports.help = {
-    name: 'archived_youtube'
+    name: 'youtube'
 }
