a901afef03736905e966bfaf6cf5b5cd166d3d7b
imsoondae <im.daeva@gmail.com>
2022-05-25 03:56:53 +0700

    new feature

--- src/commands/entertainment/translator.js
+++ src/commands/entertainment/translator.js
@@ -1,15 +1,20 @@
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed, MessageActionRow, MessageSelectMenu } = require("discord.js");
 const translate = require("@iamtraction/google-translate");
-const pagination = require("discord.js-pagination");
 
 module.exports.run = async (client, message, args) => {
     const translateQuery = args.join(" ");
     if (!translateQuery) return message.channel.send({ content: "Please specify a text to translate. Don't let it empty, like my heart." }).catch((e) => {});
 
+    message.channel.send({content: "Translating <a:_util_loading:863317596551118858>"}).then(msg => {
+        setTimeout(() => msg.delete(), 8000)
+      });
+
     const translated = await translate(translateQuery, { to: 'en' });
     const embedEN = new MessageEmbed()
         .setDescription(`<:util_googletranslate:858727960693833739> **ENGLISH**\n\n**Translation**: ${translated.text}`)
-        .setColor(`2f3136`);
+        .setColor(`2f3136`)
+        .setImage("https://i.imgur.com/77n1x09.png")
+        .setFooter({ text: `Translation request by ${message.member.displayName}`, iconURL: message.author.displayAvatarURL({ dynamic: true }) });
 
     const translatedRU = await translate(translateQuery, { to: 'ru' });
     const embedRU = new MessageEmbed()
@@ -76,28 +81,135 @@ module.exports.run = async (client, message, args) => {
         .setDescription(`<:util_googletranslate:858727960693833739> **FILIPINO**\n\n**Translation**: ${translatedTL.text}`)
         .setColor(`2f3136`);
 
-    const pages = [
-        embedEN,
-        embedJA,
-        embedKO,
-        embedFR,
-        embedIT,
-        embedDE,
-        embedRU,
-        embedTR,
-        embedTH,
-        embedID,
-        embedMS,
-        embedTL,
-        embedHI,
-        embedAR
-    ]
+    const row = new MessageActionRow()
+        .addComponents(
+            new MessageSelectMenu()
+                .setCustomId('translator-menu')
+                .setPlaceholder('Translate to ... ')
+                .setDisabled('true')
+                .addOptions([
+                    {
+                        value: 'russian',
+                        label: 'Россия',
+                        emoji: '🇷🇺'
+                    },
+                    {
+                        value: 'indonesian',
+                        label: 'Bahasa Indonesia',
+                        emoji: '🇮🇩'
+                    },
+                    {
+                        value: 'thailand',
+                        label: 'ภาษาไทย',
+                        emoji: '🇹🇭'
+                    },
+                    {
+                        value: 'franch',
+                        label: 'French',
+                        emoji: '🇫🇷'
+                    },
+                    {
+                        value: 'japanese',
+                        label: '日本',
+                        emoji: '🇯🇵'
+                    },
+                    {
+                        value: 'korean',
+                        label: '대한민국',
+                        emoji: '🇰🇷'
+                    },
+                    {
+                        value: 'turkey',
+                        label: 'Türkçe',
+                        emoji: '🇹🇷'
+                    },
+                    {
+                        value: 'arabic',
+                        label: 'عَرَبِيّ',
+                        emoji: '🇦🇪'
+                    },
+                    {
+                        value: 'hindi',
+                        label: 'हिन्दी',
+                        emoji: '🇮🇳'
+                    },
+                    {
+                        value: 'german',
+                        label: 'Deutsch',
+                        emoji: '🇩🇪'
+                    },
+                    {
+                        value: 'malaysian',
+                        label: 'Malay',
+                        emoji: '🇲🇾'
+                    },
+                    {
+                        value: 'italian',
+                        label: 'Italiano',
+                        emoji: '🇮🇹'
+                    },
+                    {
+                        value: 'tagalog',
+                        label: 'Filipino',
+                        emoji: '🇵🇭'
+                    }
+                ])
+        )
+    
+    const filter = (interaction) => interaction.isSelectMenu() && interaction.user.id === message.author.id;
 
-    const reaction = ["◀️", "▶️"]
+    const collector = message.channel.createMessageComponentCollector({ filter, max:'1' });
+    
+    collector.on("collect", async (collected) => {
+        const value = collected.values[0];
 
-    pagination(message, pages, reaction)
+        if(value === "russian" ){
+            collected.reply({ embeds: [embedRU], ephemeral: true })
+        }
+        if(value === "indonesian" ){
+            collected.reply({ embeds: [embedID], ephemeral: true })
+        }
+        if(value === "thailand" ){
+            collected.reply({ embeds: [embedTH], ephemeral: true })
+        }
+        if(value === "franch" ){
+            collected.reply({ embeds: [embedFR], ephemeral: true })
+        }
+        if(value === "japanese" ){
+            collected.reply({ embeds: [embedJA], ephemeral: true })
+        }
+        if(value === "korean" ){
+            collected.reply({ embeds: [embedKO], ephemeral: true })
+        }
+        if(value === "turkey" ){
+            collected.reply({ embeds: [embedTR], ephemeral: true })
+        }
+        if(value === "arabic" ){
+            collected.reply({ embeds: [embedAR], ephemeral: true })
+        }
+        if(value === "hindi" ){
+            collected.reply({ embeds: [embedHI], ephemeral: true })
+        }
+        if(value === "german" ){
+            collected.reply({ embeds: [embedDE], ephemeral: true })
+        }
+        if(value === "malaysian" ){
+            collected.reply({ embeds: [embedMS], ephemeral: true })
+        }
+        if(value === "italian" ){
+            collected.reply({ embeds: [embedIT], ephemeral: true })
+        }
+        if(value === "tagalog" ){
+            collected.reply({ embeds: [embedTL], ephemeral: true })
+        }
+    })
+
+    message.reply({
+        embeds: [embedEN],
+        components: [row]
+    })
 }
 
 module.exports.help = {
-    name: 'archived_translate'
+    name: 'translate'
 }
---
+++ src/commands/entertainment/wikipedia.js
@@ -0,0 +1,47 @@
+const { MessageEmbed } = require("discord.js");
+const fetch = require("node-fetch");
+
+module.exports.run = async (client, message, args) => {
+    const wiki = args.join(" ");
+    if (!wiki) return message.reply("By the moonlight, what you seeks for?").catch((e) => {});
+
+    const url = `https://en.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(wiki)}`
+
+    let response
+    try {
+        response = await fetch(url).then(res => res.json())
+    }
+    catch (e) {
+        return message.reply('Something hit my head and make it error..')
+    }
+
+    try {
+        if(response.type === 'disambiguation') {
+            const embed = new MessageEmbed()
+                .setTitle(response.title)
+                .setURL(response.content_urls.desktop.page)
+                .setDescription([`\n${response.extract}\n[Links for topic you searched](${response.content_urls.desktop.page})`])
+                .setColor(`2f3136`)
+                .setFooter({ text: `Wikipedia`, iconURL:'https://i.imgur.com/OX1OJcd.png' })
+            message.reply({ embeds: [embed] }).catch((e) => {});
+        }
+        else {
+            const embed = new MessageEmbed()
+                .setTitle(response.title)
+                .setURL(response.content_urls.desktop.page)
+                .setThumbnail(response.thumbnail.source)
+                .setDescription(response.extract)
+                .setColor(`2f3136`)
+                .setFooter({ text: `Wikipedia`, iconURL:'https://i.imgur.com/OX1OJcd.png' })
+            message.reply({ embeds: [embed] }).catch((e) => {});
+        }
+    }
+    catch {
+        return message.reply("My knowledge can't define the word further, try seek another wisdom")
+    }
+}
+
+module.exports.help = {
+    name: 'wikipedia',
+    aliases: ['wiki']
+}
