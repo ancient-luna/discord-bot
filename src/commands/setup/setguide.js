6e4f2b731a3434181bae81c2c604929795e8c4dc
imsoondae <im.daeva@gmail.com>
2022-05-31 08:15:51 +0700

    button added, more clean

--- src/commands/setup/setguide.js
+++ src/commands/setup/setguide.js
@@ -1,4 +1,4 @@
-const { MessageEmbed, MessageAttachment, MessageActionRow } = require('discord.js');
+const { MessageEmbed, MessageAttachment, MessageActionRow, MessageButton } = require('discord.js');
 
 module.exports.run = async (client, message, args) => {
     if (!message.member.permissions.has("MANAGE_MESSAGES")) return;
@@ -33,6 +33,14 @@ module.exports.run = async (client, message, args) => {
     let embedRoles = new MessageEmbed()
         .setDescription(`You can apply to get this role by open a ticket for application:\n\n⁣The <@&907178060992876544> role only given to guild members in Toram Online game,\nwhile <@&873872221368647690> role given to guild alliances members\n\nReact to any reaction that suits you for the game you love:\n\n${ToramOnlineEmoji} <@&952147085447266364> for Toram Online\n⁣${BlackDesertOnlineEmoji} <@&856380073745186876> for Black Desert Online\n${ApexLegendsEmoji} <@&861400119101095937> for Apex Legends\n${DeadFrontierEmoji} <@&874680389459906580> for Dead Frontier\n\n${AdAstraAbyssosqueEmoji} <@&882350441864777769> for unlocking nsfw contents\n\nBy this you will unlock the hidden category in this server to meet another light seekers in this sanctuary`)
         .setColor("#2f3136")
+              
+    const emblemToram = new MessageActionRow()
+        .addComponents(
+            new MessageButton()
+                .setStyle("LINK")
+                .setLabel("Get your emblem here")
+                .setURL("https://discord.com/channels/447069790150852609/952170767947272303")
+        )
 
     await message.channel.send({ files: [IMGguide], embeds: [embedGuide] }).catch((e) => {});
     await message.channel.send({ files: [IMGticket], embeds: [embedTicket] }).catch((e) => {});
@@ -51,7 +59,10 @@ module.exports.run = async (client, message, args) => {
         if (reaction.message.channel.id === channelServerRole) {
             if (reaction.emoji.name === 'game_logo_toram') {
                 await reaction.message.guild.members.cache.get(user.id).roles.add(ToramOnlineRole).catch((e) => {});
-                message.guild.channels.cache.get('952164768217706496').send(`A legendary saviour known as ${reaction.message.guild.members.cache.get(user.id)} just departured from Iruna to this ancient city of luna\nHow about to check out your <#952170767947272303> before go further <:xpot_toram_potum_cute_chilling:952260990085500978>`).catch((e) => {});
+                message.guild.channels.cache.get('952164768217706496').send({
+                    content: `A legendary saviour known as ${reaction.message.guild.members.cache.get(user.id)} just departured from Iruna to this ancient city of luna <:xpot_toram_potum_cute_chilling:952260990085500978>`,
+                    components: [emblemToram]
+                }).catch((e) => {});
             }
             if (reaction.emoji.name === 'game_logo_bdo') {
                 await reaction.message.guild.members.cache.get(user.id).roles.add(BlackDesertOnlineRole).catch((e) => {});
