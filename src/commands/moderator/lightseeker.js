a702f7410ff6bd0444d5593cd7ad56a4a697e6b9
imsoondae <im.daeva@gmail.com>
2022-05-31 08:15:40 +0700

    update format

--- src/commands/moderator/lightseeker.js
+++ src/commands/moderator/lightseeker.js
@@ -1,3 +1,5 @@
+const { MessageActionRow, MessageButton } = require("discord.js");
+
 module.exports.run = async (Client, message, args) => {
     if (!message.member.permissions.has("MANAGE_MESSAGES")) return;
 
@@ -9,7 +11,18 @@ module.exports.run = async (Client, message, args) => {
 
     if (!role) return message.reply('please mention a role after user').catch((e) => {});
 
-    message.guild.channels.cache.get('452842830776369152').send(`<@${target.user.id}> has passed the trial by understand our wisdom of lleud to reach this warm sanctuary deeper.\nWelcome, to the sanctuary of lights. The <@&843523544620335124> welcome you as one of true **light seekers**. <:ancientluna_pure_luna:866781517312688178>`).then(target.roles.add(role)).catch((e) => {});
+    const welcomeButton = new MessageActionRow()
+        .addComponents(
+            new MessageButton()
+                .setStyle("LINK")
+                .setLabel("Get more roles here")
+                .setURL("https://discord.com/channels/447069790150852609/864556584818835456")
+    )
+
+    message.guild.channels.cache.get('452842830776369152').send({
+        content: `<@${message.author.id}> has passed the trial by understand our wisdom of lleud to reach this warm sanctuary deeper.\nWelcome, to the sanctuary of lights. The <@&${gConfig.server.elderRole}> welcome you as one of true light seekers ${ancientLunaEmoji}`,
+        components: [welcomeButton]
+    }).catch((e) => {});
     
     await message.delete().catch((e) => {});
 }
