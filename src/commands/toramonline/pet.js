20ef545896cae0151322fb3720d7a803d6254d08
imsoondae <im.daeva@gmail.com>
2022-05-31 08:15:58 +0700

    new feature (pet list)

---
+++ src/commands/toramonline/pet.js
@@ -0,0 +1,39 @@
+const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
+
+module.exports.run = async (Client, message, args) => {
+    const ar = "<a:_util_arrow:864810269771300875>"
+
+    let pet = new MessageEmbed()
+        .setTitle("Pet List")
+        .setURL("https://discord.com/invite/Sbp2nt8QHe")
+        .setDescription(`All documents are all from **[さるびあのトーラムメモ](https://slv-memo.space/)**`)
+        .setTimestamp()
+        .setFooter({ text: "Powered by Ancient Luna", iconURL: 'https://i.imgur.com/QZ2gLgq.png' })
+        .setColor("#2f3136")
+
+    
+    const petlist = new MessageActionRow()
+        .addComponents(
+            new MessageButton()
+                .setStyle("LINK")
+                .setLabel("Normal Pet List")
+                .setURL("https://slv-memo.space/pet/pet-list")
+        )
+
+        .addComponents(
+            new MessageButton()
+                .setStyle("LINK")
+                .setLabel("Event Pet List")
+                .setURL("https://slv-memo.space/pet/pet-list-event")
+        )
+
+    await message.reply({
+        embeds: [pet],
+        components: [petlist]
+    }).catch((e) => {});
+}
+
+module.exports.help = {
+    name: 'pet',
+    aliases: ['pets']
+}
