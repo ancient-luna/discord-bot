1784c222839ae8bc2be2b769ebe6928b5806a9d1
imsoondae <im.daeva@gmail.com>
2022-06-01 16:06:09 +0700

    adding new roles

--- src/commands/setup/setguide.js
+++ src/commands/setup/setguide.js
@@ -11,6 +11,7 @@ module.exports.run = async (client, message, args) => {
     const DeadFrontierRole = message.guild.roles.cache.find(role => role.name === 'Survivors')
     const AdAstraAbyssosqueRole = message.guild.roles.cache.find(role => role.name === 'Ad Astra Abyssosque')
     const MinecraftRole = message.guild.roles.cache.find(role => role.name === 'Mooncraft')
+    const ValorRole = message.guild.roles.cache.find(role => role.name === 'Valor')
 
     const ToramOnlineEmoji = '<:game_logo_toram:952247863075823666>';
     const BlackDesertOnlineEmoji = '<:game_logo_bdo:861579805660151818>';
@@ -18,6 +19,7 @@ module.exports.run = async (client, message, args) => {
     const DeadFrontierEmoji = '<:game_logo_df:861580085000798229>';
     const AdAstraAbyssosqueEmoji = '<:ancientluna_divinare_s:859034096192978965>';
     const MooncraftEmoji = '<:game_logo_mc:981470249792712774>';
+    const ValorEmoji = '<:game_logo_valor:981481044874252338>';
 
     const IMGguide = new MessageAttachment("src/assets/guidelines.png")
     const IMGticket = new MessageAttachment("src/assets/ticketopen.png")
@@ -33,7 +35,7 @@ module.exports.run = async (client, message, args) => {
         .setColor("#2f3136")
 
     let embedRoles = new MessageEmbed()
-        .setDescription(`You can apply to get this role by open a ticket for application:\n\n⁣The <@&907178060992876544> role only given to guild members in Toram Online game,\nwhile <@&873872221368647690> role given to guild alliances members\n\nReact to any reaction that suits you for the game you love:\n\n${ToramOnlineEmoji} <@&952147085447266364> for Toram Online\n⁣${BlackDesertOnlineEmoji} <@&856380073745186876> for Black Desert Online\n${ApexLegendsEmoji} <@&861400119101095937> for Apex Legends\n${DeadFrontierEmoji} <@&874680389459906580> for Dead Frontier\n${MooncraftEmoji} for Minecraft\n\n${AdAstraAbyssosqueEmoji} <@&882350441864777769> for unlocking nsfw contents\n\nBy this you will unlock the hidden category in this server to meet another light seekers in this sanctuary`)
+        .setDescription(`You can apply to get this role by open a ticket for application:\n\n⁣The <@&907178060992876544> role only given to guild members in Toram Online game,\nwhile <@&873872221368647690> role given to guild alliances members\n\nReact to any reaction that suits you for the game you love:\n\n${ToramOnlineEmoji} <@&952147085447266364> for Toram Online\n⁣${BlackDesertOnlineEmoji} <@&856380073745186876> for Black Desert Online\n\n${ApexLegendsEmoji} <@&861400119101095937> for Apex Legends\n${DeadFrontierEmoji} <@&874680389459906580> for Dead Frontier\n${ValorEmoji} <@&981479474531024958> for Valorant\n\n${MooncraftEmoji} <@&981470521470382090> for Minecraft\n\n${AdAstraAbyssosqueEmoji} <@&882350441864777769> for unlocking nsfw contents\n\nBy this you will unlock the hidden category in this server to meet another light seekers in this sanctuary`)
         .setColor("#2f3136")
               
     const emblemToram = new MessageActionRow()
@@ -53,6 +55,7 @@ module.exports.run = async (client, message, args) => {
     messageEmbed.react(DeadFrontierEmoji).catch((e) => {});
     messageEmbed.react(AdAstraAbyssosqueEmoji).catch((e) => {});
     messageEmbed.react(MooncraftEmoji).catch((e) => {});
+    messageEmbed.react(ValorEmoji).catch((e) => {});
 
     client.on('messageReactionAdd', async (reaction, user) => {
         if (reaction.message) await reaction.message.fetch().catch((e) => {});
@@ -81,6 +84,9 @@ module.exports.run = async (client, message, args) => {
             }
             if (reaction.emoji.name === 'game_logo_mc') {
                 await reaction.message.guild.members.cache.get(user.id).roles.add(MinecraftRole).catch((e) => {});
+            }
+            if (reaction.emoji.name === 'game_logo_valor') {
+                await reaction.message.guild.members.cache.get(user.id).roles.add(ValorRole).catch((e) => {});
             }
         } else {
             return;
@@ -111,6 +117,9 @@ module.exports.run = async (client, message, args) => {
             }
             if (reaction.emoji.name === 'game_logo_mc') {
                 await reaction.message.guild.members.cache.get(user.id).roles.remove(MinecraftRole).catch((e) => {});
+            }
+            if (reaction.emoji.name === 'game_logo_valor') {
+                await reaction.message.guild.members.cache.get(user.id).roles.remove(ValorRole).catch((e) => {});
             }
         } else {
             return;
