50dc665c6bc6cb00b3fac899eb8b99231bfc969c
imsoondae <im.daeva@gmail.com>
2022-06-08 12:16:33 +0700

    new presence

--- src/index.js
+++ src/index.js
@@ -66,7 +66,7 @@ client.on('ready', async () => {
 
   client.user.setPresence({
     activities: [{
-      name: `in Smol Lazy Cats`,
+      name: `with cutie colons`,
       type: `PLAYING`,
     }],
     status: `online`
