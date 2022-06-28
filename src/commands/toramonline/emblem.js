649ae9f2e4b83551e7dd0a7190f0e42e9c68d5a3
imsoondae <im.daeva@gmail.com>
2022-06-28 21:27:08 +0700

    Update emblem.js

--- src/commands/toramonline/emblem.js
+++ src/commands/toramonline/emblem.js
@@ -34,19 +34,19 @@ module.exports.run = async (Client, message, args) => {
     let embedWielder = new MessageEmbed()
         .setDescription(`${BOWemoji} - **Bow wielder** wind caller\n${CBemoji} - As sneaky **Bowgun wielder**\n${DSemoji} - **Dual Sword wielder** master\n${KNUCKemoji} - Short **Knuckle wielder** operator \n${HBemoji} - The **Halberd wielder** seeker\n${KTNemoji} - Bloomy light **Katana wielder**\n${OHSemoji} - **One-handed Sword wielder** saviour\n${THSemoji} - Heartless **Two-Handed Sword wielder**\n${STAFFemoji} - Unstoppable **Staff wielder** magician`)
         .setThumbnail("https://i.imgur.com/9iCRGhG.png")
-        .setColor("#2f3136")
+        .setColor("2f3136")
 
     let embedSaviour = new MessageEmbed()
         .setDescription(`${DPSemoji} - **DPS** Focus\n\n*It is used as a metric in journey to allow a determination of offensive power for their ability to destroy and eliminate.*\n\n${TANKemoji} - Bravery **Tanker**\n\n*A saviour that draws attackers away from other players. They’re typically burly warriors, knights, or paladins that most likely gained their nickname from their big muscles. They tend to be decked out in battle-worn steel, like a living tanker.*\n\n${SUPPORTemoji} - **Support** Specialist\n\n*Usually had one role in the group: Make everyone else better. This wasn’t the easiest role to take on for many reasons. It’s difficult to be the class that doesn’t actively do something like do the most damage or ensure no one else gets hit.*`)
-        .setColor("#2f3136")
+        .setColor("2f3136")
 
     let embedJourney = new MessageEmbed()
         .setDescription(`${STOODIEemoji} - **Stoodie's Experiment** Dungeon and Regislets\n${HUNTemoji} - Companions from all over the world as **Treasures Hunter** with Pelulu Keys\n${MATSemoji} - **Restless Farm Materials** night. Any materials will do, just anything`)
-        .setColor("#2f3136")
+        .setColor("2f3136")
 
     let embedMemoir = new MessageEmbed()
         .setDescription(`${TIMEemoji} - Reminder for food buffland to water, harvest, and cook. **Daydreamer**'s dream\n${NOTEemoji} - A humble **Librarian** that know how knowledges help other lost saviours`)
-        .setColor("#2f3136")
+        .setColor("2f3136")
 
     await message.channel.send({ files: [IMGwielder], embeds: [embedWielder] }).catch(e => {});
     await message.channel.send({ files: [IMGsaviour], embeds: [embedSaviour] }).catch(e => {});
