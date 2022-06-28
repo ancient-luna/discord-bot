bd963258124c20da1780ecf848a5d22f0d3d91e3
imsoondae <im.daeva@gmail.com>
2022-06-28 10:15:10 +0700

    rawrrrr

--- src/commands/entertainment/rawr.js
+++ src/commands/entertainment/rawr.js
@@ -5,7 +5,7 @@ module.exports.run = async (client, message, args) => {
     let IMGrawr = new MessageAttachment("src/assets/react/rawrrr.png")
 
     await message.channel.send({
-        content: "+1 RaawWwRrrRRrrrrRR~ for <@162552850432393216>\n⁣",
+        content: "scawy RaawWwRrrRRrrrrRR~ from <@162552850432393216>\n⁣",
         files: [IMGrawr]
     }).catch((e) => {});
 }
