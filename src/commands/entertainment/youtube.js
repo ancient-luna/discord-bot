4a167d32c68c5c38dd3ccfcecdcbcf2970063764
imsoondae <im.daeva@gmail.com>
2022-06-28 10:15:49 +0700

    aiyo no idea la but ushusgh work

--- src/commands/entertainment/youtube.js
+++ src/commands/entertainment/youtube.js
@@ -7,7 +7,7 @@ module.exports.run = async (Client, message, args) => {
     if(!channel) return message.reply(`You have to be in any **voice channel** first in this server`).catch((e) => {});
 
     discordTogether
-        .createTogetherCode(channel.id, 'youtube')
+        .createTogetherCode(channel.id, 'youtubeDev')
         .then((invite) => 
         
             message.reply({
