8c1f4e7ccb73b09abe07ac5af63874bfbb6a74a6
imsoondae <im.daeva@gmail.com>
2022-07-16 16:58:15 +0700

    fix Role

--- src/commands/moderator/lightseeker.js
+++ src/commands/moderator/lightseeker.js
@@ -20,7 +20,7 @@ module.exports.run = async (Client, message, args) => {
     )
 
     message.guild.channels.cache.get('452842830776369152').send({
-        content: `<@${target.user.id}> has passed the trial by understand our wisdom of lleud to reach this warm sanctuary deeper.\nWelcome, to the sanctuary of lights. The <@&843523544620335124> welcome you as one of true light seekers <:ancientluna_pure_luna:866781517312688178>`,
+        content: `<@${target.user.id}> has passed the trial by understand our wisdom of lleud to reach this warm sanctuary deeper.\nWelcome, to the sanctuary of lights. The <@&843523544620335124> welcome you as one of true light seekers <:ancientluna_pure_luna:866781517312688178>`.then(target.roles.add(role)).catch(e => {}),
         components: [welcomeButton]
     }).catch((e) => {});
     
