520c54b5f4ac7ef436b925b59598668624c18cbb
imsoondae <im.daeva@gmail.com>
2022-07-17 11:37:07 +0700

    Create doubt.js

---
+++ src/commands/hogwarts/doubt.js
@@ -0,0 +1,16 @@
+module.exports.run = async (client, message, args) => {
+    if (!message.member.permissions.has("MANAGE_MESSAGES")) return;
+    await message.delete().catch((e) => {});
+
+    message.guild.channels.cache.get('997467514818797649').send(`*<@${message.author.id}> failed to take the test as themself not ready ...*`).catch(e => {});
+
+    if (!message.channel.name.includes('sorting')) return message.reply(`*My authorithy as The Sorting Hat's right hand can't do further to doubt and delete this channel of mind*`).catch((e) => {});
+    message.channel.send({ content: "*Doubting you.... It's really hard to chose cause you weren't ready. I'll see you once you ready later ..." }).catch((e) => {});
+    setTimeout(() => {
+        message.channel.delete().catch((e) => {});
+    }, 5000)
+}
+
+module.exports.help = {
+    name: 'doubt'
+}
