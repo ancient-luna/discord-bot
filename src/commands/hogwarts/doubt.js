117be6119317110c2cfe3e79e5bc796161bcdc41
imsoondae <im.daeva@gmail.com>
2022-07-18 17:38:43 +0700

    It never doubt

--- src/commands/hogwarts/doubt.js
+++ src/commands/hogwarts/doubt.js
@@ -5,7 +5,7 @@ module.exports.run = async (client, message, args) => {
     message.guild.channels.cache.get('997467514818797649').send(`*Someone has failed to take the test as themself was not ready ...*`).catch(e => {});
 
     if (!message.channel.name.includes('sorting')) return message.reply(`*My authorithy as The Sorting Hat's right hand can't do further to doubt and delete this channel of mind*`).catch((e) => {});
-    message.channel.send({ content: "*Doubting you.... It's really hard to chose 'cause you weren't ready. I'll see you once you're ready later ..." }).catch((e) => {});
+    message.channel.send({ content: "*Doubting you.... It's really hard to chose 'cause you weren't ready. I'll see you once you're ready later ...*" }).catch((e) => {});
     setTimeout(() => {
         message.channel.delete().catch((e) => {});
     }, 5000)
