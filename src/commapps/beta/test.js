c452002c32083a36c04bad0bafa44f257aff81ea
imsoondae <im.daeva@gmail.com>
2022-07-25 03:33:20 +0700

    im yours to tame

--- src/commapps/beta/test.js
+++ src/commapps/beta/test.js
@@ -3,8 +3,8 @@ const { SlashCommandBuilder } = require('@discordjs/builders')
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('test')
-    .setDescription('SLASH command desc'),
+    .setDescription('jsut call my name im yours to tame'),
     async execute(interaction, client) {
-        await interaction.reply({ content: 'Oceans & Engines - NIKI'})
+        await interaction.reply({ content: 'https://open.spotify.com/track/58HvfVOeJY7lUuCqF0m3ly?si=8197fa2e6edb45b4'})
     }
 }
