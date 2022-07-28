ebb84c57c5ddf806dd8654b62ee29b127e313555
imsoondae <im.daeva@gmail.com>
2022-07-28 21:51:15 +0700

    Update interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,14 +1,39 @@
-const { Interaction } = require("discord.js");
-
 module.exports = {
     name: 'interactionCreate',
     async execute(interaction, client) {
-        if (!interaction.isCommand()) return;
+        if (interaction.isCommand()) handleSlashCommand(interaction, client);
+        else if (interaction.isButton()) handleButton(interaction, client);
+    },
+}
 
-        const command = client.commands.get(interaction.commandName);
+const handleButton = (interaction, client) => {
+    
+    const [name, ...params] = interaction.customId.split("-")
 
-        if (!command) return
+    const button = client.buttons.get(name)
 
+    if (!button) return
+
+    (async () => {
+        try {
+            await button.run(interaction, client, params);
+        } catch (error) {
+            console.log(error);
+            await interaction.reply({
+                content: 'There was an error while executing this button!',
+                ephemeral: true
+            });
+        }
+    })();
+}
+
+const handleSlashCommand = (interaction, client) => {
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return
+
+    (async () => {
         try {
             await command.execute(interaction, client);
         } catch (error) {
@@ -18,5 +43,5 @@ module.exports = {
                 ephemeral: true
             });
         }
-    },
-};
+    })();
+}
