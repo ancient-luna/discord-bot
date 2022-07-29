690fe7e9a5b4ea035ec941322020493f5ea20dad
imsoondae <im.daeva@gmail.com>
2022-07-30 03:53:16 +0700

    Update lightseeker.js

--- src/commands/moderator/lightseeker.js
+++ src/commands/moderator/lightseeker.js
@@ -11,6 +11,8 @@ module.exports.run = async (Client, message, args) => {
 
     if (!role) return message.reply('please mention a role after user').catch((e) => {});
 
+    await message.delete().catch((e) => {});
+
     const welcomeButton = new MessageActionRow()
         .addComponents(
             new MessageButton()
@@ -20,11 +22,10 @@ module.exports.run = async (Client, message, args) => {
     )
 
     message.guild.channels.cache.get('452842830776369152').send({
-        content: `<@${target.user.id}> has passed the trial by understand our wisdom of lleud to reach this warm sanctuary deeper.\nWelcome, to the sanctuary of lights. The <@&843523544620335124> welcome you as one of true light seekers <:ancientluna_pure_luna:866781517312688178>`.then(target.roles.add(role)).catch(e => {}),
+        content: `<@${target.user.id}> has passed the trial by understand our wisdom of lleud to reach this warm sanctuary deeper.\nWelcome, to the sanctuary of lights. The <@&843523544620335124> welcome you as one of true light seekers <:ancientluna_pure_luna:866781517312688178>`,
         components: [welcomeButton]
-    }).catch((e) => {});
+    }).then(target.roles.add(role)).catch((e) => {});
     
-    await message.delete().catch((e) => {});
 }
 
 module.exports.help = {
