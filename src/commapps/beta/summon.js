c6492a0a1ef06efcbfe8d653c74bd700b8877350
imsoondae <im.daeva@gmail.com>
2022-08-02 16:13:09 +0700

    your lies too sweet

--- src/commapps/beta/summon.js
+++ src/commapps/beta/summon.js
@@ -8,7 +8,7 @@ module.exports = {
     async execute(interaction, client) {
         
         await interaction.reply({
-            content: 'i summoned, wow!'
+            content: `_ _\nDon't leave\nSay I'm the only one you need\nSay I'm the only secret you keep\nDon't break me, don't break me down\n\n*[do you even realize?](https://open.spotify.com/track/5ngJKkOmjkN460b2ApBnLk?si=24e1844ec49b4db3)*\n\nNo, no-no, no, no-no-no-no\nI'm coming home even if I'm alone\nKnow that it's wrong\nBut I still wish you were here\nBaby, come here\n\nDon't break me down\nI don't wanna lose you now\n⁣`
         }).catch((e) => {});
     }
 }
