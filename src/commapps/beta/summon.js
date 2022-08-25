ace087155bf22074723c843674a4c7b226648f05
imsoondae <im.daeva@gmail.com>
2022-08-26 07:12:06 +0900

    update content

--- src/commapps/beta/summon.js
+++ src/commapps/beta/summon.js
@@ -8,7 +8,7 @@ module.exports = {
     async execute(interaction, client) {
         
         await interaction.reply({
-            content: `_ _\nDon't leave\nSay I'm the only one you need\nSay I'm the only secret you keep\nDon't break me, don't break me down\n\n*[do you even realize?](https://open.spotify.com/track/5ngJKkOmjkN460b2ApBnLk?si=24e1844ec49b4db3)*\n\nNo, no-no, no, no-no-no-no\nI'm coming home even if I'm alone\nKnow that it's wrong\nBut I still wish you were here\nBaby, come here\n\nDon't break me down\nI don't wanna lose you now\n⁣`
+            content: `달빛 아래 떠오르는 [web](<https://ancientluna.org/>) . [support](<https://ko-fi.com/daekid>) . [discord](<https://discord.com/invite/Sbp2nt8QHe>) . [youtube](https://www.youtube.com/watch?v=rPnPdPTsFxs)\nI'm a relic that born to seek wisdom ╮`
         }).catch((e) => {});
     }
 }
