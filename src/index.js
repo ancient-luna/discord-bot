1d83eaa645426a6b4bd0d859a1d5a64aa950ca77
imsoondae <im.daeva@gmail.com>
2022-12-11 23:02:31 +0700

    civilizatsyions

--- src/index.js
+++ src/index.js
@@ -228,7 +228,7 @@ client.on('messageReactionAdd', async (reaction, user) => {
       await reaction.message.guild.members.cache.get(user.id).roles.add('856380073745186876')
       const channelCity = reaction.message.guild.channels.cache.get(bdoChannelId);
       channelCity.send({
-        content: `Welcome to the hidden secrets of the ancient civilizations ${reaction.message.guild.members.cache.get(user.id)} ,\nA journey to seek the true face of the ancient civilization around the Black Desert awaits you!`
+        content: `Welcome to the hidden secrets of the ancient civilizations ${reaction.message.guild.members.cache.get(user.id)} ,\nA journey to seek the true face of the <#1049815440198733895> around the Black Desert awaits you!`
       })
     }
     if (reaction.emoji.name === 'game_logo_apex') {
