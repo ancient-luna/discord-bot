28042eff3d7ce4939c1acf481d7dc80b64556468
imsoondae <im.daeva@gmail.com>
2023-01-07 02:09:56 +0700

    Update disciple.js

--- src/commands/blackdesert/disciple.js
+++ src/commands/blackdesert/disciple.js
@@ -11,13 +11,13 @@ module.exports.run = async (Client, message, args) => {
 
     if (!role) return message.reply('please mention a role after user').catch(e => {});
 
-    message.guild.channels.cache.get('1060992670035619931').send(``).then(target.roles.add(role)).catch(e => {});
+    message.guild.channels.cache.get('1060992670035619931').send(`A seeker named <@${target.user.id}> become a part of <@&1060982357538119850> [<:ancientluna_pure_luna:866781517312688178>]`).then(target.roles.add(role)).catch(e => {});
     
     await message.delete().catch((e) => {});
 
     const addEOS = new MessageEmbed()
         .setAuthor({ name: "ROLE ADDED", iconURL: "https://i.imgur.com/ejkkWCB.png" })
-        .setDescription("You have been gived **Lunar Disciples** role and have access to all channels as an official member in **[Ecplise Boarder Hall](https://discord.com/channels/447069790150852609/1060992670035619931)** category. May the lights guide us, so we may bask in its light as a true ancient civilizations")
+        .setDescription("You have been gived **Lunar Disciples** role and have access to all channels as an official guild member in **[Ecplise Boarder Hall](https://discord.com/channels/447069790150852609/1060992670035619931)** category. May the lights guide us, so we may bask in its light as a true ancient civilizations")
         .setTimestamp()
         .setColor("2f3136")
         .setFooter({ text: "Ancient Luna Guild: We ran as if to meet the moon" })
