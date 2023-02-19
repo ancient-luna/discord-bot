62df4c9dfc09fa72af9830a5f78fc4dbaa0dfcca
imsoondae <im.daeva@gmail.com>
2023-02-19 13:25:02 +0700

    its more e m o s y i o n a l d e m e j i

--- src/commands/entertainment/emotionaldamage.js
+++ src/commands/entertainment/emotionaldamage.js
@@ -7,7 +7,7 @@ module.exports.run = async (client, message, args) => {
     let SRCtomato = new MessageAttachment("src/assets/react/tomatooo.mp4")
 
     await loadingTxt.edit({
-        content: "+9999999 destruction by flying smol piece of toma'o <:xemot_meow_fine_fire:1063723426155606067>",
+        content: "+9999999 destruction by flying smol piece of <@946058733954433085> <:xemot_meow_fine_fire:1063723426155606067>",
         files: [SRCtomato]
     }).then((msg) => {
         msg.react('🔥').catch((e) => {});
