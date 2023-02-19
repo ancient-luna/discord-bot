b47c741892bcfcb2a236fbaec19b54e8208aa62e
imsoondae <im.daeva@gmail.com>
2023-02-19 15:17:34 +0700

    vacancy letter

---
+++ src/commands/blackdesert/absence.js
@@ -0,0 +1,46 @@
+const { MessageActionRow, MessageButton, MessageEmbed } = require("discord.js");
+
+module.exports.run = async (Client, message, args) => {
+
+    const elderHall = '1060992670035619931'
+
+    if (!elderHall.includes(message.channel.id)) return message.channel.send(`*You can't send a vacancy letter from here, go back to <#1060992670035619931> to send!*`).then((msg) => {
+        setTimeout(() => msg.delete().catch((e) => {}), 5000);
+        setTimeout(() => message.delete().catch((e) => {}));
+    }).catch((err) => {
+        throw err;
+    })
+
+    if (!args[0]) return message.channel.send(`*You can't let the vacancy letter be an empty-ink letter*`).then((msg) => {
+        setTimeout(() => msg.delete().catch((e) => {}), 5000);
+        setTimeout(() => message.delete().catch((e) => {}));
+    }).catch((err) => {
+        throw err;
+    })
+    
+    const words = args.slice(0).join(" ");
+
+    await message.delete().catch((e) => {});
+
+    const absenceNote = new MessageEmbed()
+        .setAuthor({ name: "Vacancy Letter" })
+        .setDescription(`*Dear Elders,\n\n${words}\n\nYours sincerely,\n${message.member.displayName}*`)
+        .setColor('2f3136')
+        .setThumbnail('https://i.imgur.com/EpDcu9d.png')
+
+    if (message.channel.id === elderHall) {
+        message.guild.channels.cache.get('1076767724224659526').send({ embeds: [absenceNote] }).catch((e) => {});
+        message.channel.send(`*The vacancy letter has been delivered to the Elders. All your privacy will be kept safe under them*`).then((msg) => {
+            setTimeout(() => msg.delete().catch((e) => {}), 5000);
+            setTimeout(() => message.delete().catch((e) => {}));
+        }).catch((err) => {
+            throw err;
+        })
+    } else {
+        return;
+    }
+}
+
+module.exports.help = {
+    name: 'absence'
+}
