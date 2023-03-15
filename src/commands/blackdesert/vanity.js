04cfd1a68503423812f4821a1b4cf7b2e64751da
imsoondae <im.daeva@gmail.com>
2023-03-15 09:04:47 +0700

    n(?)

--- src/commands/blackdesert/vanity.js
+++ src/commands/blackdesert/vanity.js
@@ -8,7 +8,7 @@ module.exports.run = async (Client, message, args) => {
     const IMGVanity = new MessageAttachment("src/assets/bdo/civilization_vanity.png")
 
     let embedVanity = new MessageEmbed()
-        .setDescription(`<:xu_boss_kzarka:1085378897559957504> Kzarka <:xu_boss_kutum:1085378907806642286> Kutum <:xu_boss_nouver:1085378901510979594> Nouver <:xu_boss_karanda:1085378915234742353> Karanda <:xu_boss_murakanquint:1085378928585216143> Muraka & Quint <:xu_boss_offin:1085378921991770202> Offin <:xu_boss_garmoth:1085378935136718928> Garmoth <:xu_boss_vell:1085381103403147344> Vell`)
+        .setDescription(`<:xu_boss_kzarka:1085378897559957504> Kzarka <:xu_boss_kutum:1085378907806642286> Kutum <:xu_boss_nouver:1085378901510979594> Nouver <:xu_boss_karanda:1085378915234742353> Karanda <:xu_boss_murakanquint:1085378928585216143> Muraka & Quint <:xu_boss_offin:1085378921991770202>\nOffin <:xu_boss_garmoth:1085378935136718928> Garmoth <:xu_boss_vell:1085381103403147344> Vell`)
         .setColor("2f3136")
 
     await message.channel.send({ files: [IMGVanity], embeds: [embedVanity] }).catch(e => {});
