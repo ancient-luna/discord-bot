d78061c6ce5512ceec929b2280cb8b2f89256496
imsoondae <im.daeva@gmail.com>
2023-04-13 07:07:38 +0700

    only in arsha

--- src/commands/blackdesert/disciple.js
+++ src/commands/blackdesert/disciple.js
@@ -13,7 +13,7 @@ module.exports.run = async (Client, message, args) => {
         .setAuthor({ name: "Ancient Luna Guild Terms", iconURL: "https://i.imgur.com/SOCuup9.png" })
         .setDescription("Welcome to the sanctuary of lights\nA home for the light seekers!")
         .addFields(
-            { name: `**Street Fight / PVP**`, value: `<:_1:1075437107704778943> Killing is OK only in arsha 😦 wanted free kill but players easily make drama\n<:_2:1075437110183604355> Avoid bad manner and dont ever trash talking in any servers\n<:_3:1075437114721837157> For spots \`normal server\` you can kill but ask for DFS first \`arsha server\` you can kill without DFS of course`, inline: false },
+            { name: `**Street Fight / PVP**`, value: `<:_1:1075437107704778943> Killing is OK __**only** in arsha__ 😦 ~~wanted free kill but players easily make drama~~\n<:_2:1075437110183604355> Avoid bad manner and dont ever trash talking in any servers\n<:_3:1075437114721837157> For spots \`normal server\` you can kill but ask for DFS first \`arsha server\` you can kill without DFS of course`, inline: false },
             { name: `**Guild Quests**`, value: `<:_1:1075437107704778943> Guild Quests only can be taken for (Large) size only\n<:_2:1075437110183604355> SMH and Combat GQs can be taken around time 00.00 - 00.00 GMT+8\n<:_3:1075437114721837157> Life GQs can be taken around time 19.00 - 00.00 GMT+8`, inline: false },
             { name: `**Vacation / Day-Off**`, value: `If you are unable to login for 7days straight it is  a must to let know so we may not kick you out from the guild.\nDo \`!absence days reason\` in <#1060992670035619931>\nex: !absence 14 im having work trip and also burned out of this endless grind game`, inline: false }
         )
