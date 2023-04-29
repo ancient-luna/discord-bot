0669f9efb1d795ff05d997ec308919ebc10d78c6
imsoondae <im.daeva@gmail.com>
2023-04-30 01:53:56 +0700

    the new calls

--- src/commands/deadfrontier/thecalls.js
+++ src/commands/deadfrontier/thecalls.js
@@ -8,7 +8,7 @@ module.exports.run = async (Client, message, args) => {
     const IMGthecalls = new MessageAttachment("src/assets/df/thecalls.png")
 
     let embedCalls = new MessageEmbed()
-        .setDescription(`<:zdf_call_outpostattack:1101942716104974438> Outpost Attacks\n<:zdf_call_mission:1101942084056911995> Missions\n<:zdf_call_bandit:1101941930620899452> Bandit Raid\n<:zdf_call_devilhound:1101941570640556103> Devil Hounds\n<:zdf_call_volatileleaper:1101941601808425130> Volatile Leapers`)
+        .setDescription(`<:zdf_call_outpostattack:1101942716104974438> Outpost Attack Shouter<:zdf_call_mission:1101942084056911995> Missions Specialist\n<:zdf_call_bandit:1101941930620899452> Bandit Raid <:zdf_call_devilhound:1101941570640556103> Devil Hounds Slayer <:zdf_call_volatileleaper:1101941601808425130> Volatile Leapers Hunter`)
         .setColor("2f3136")
 
     await message.channel.send({ files: [IMGthecalls], embeds: [embedCalls] }).catch(e => {});
