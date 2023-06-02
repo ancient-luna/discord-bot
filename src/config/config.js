a10b99eec041f72111e9fbeb35a3805fbf319978
imsoondae <im.daeva@gmail.com>
2023-06-02 22:42:40 +0700

    app id public

--- src/config/config.js
+++ src/config/config.js
@@ -16,11 +16,11 @@ module.exports = {
     voiceMessage: "boss cycle updated",
 
     color: '#4277e8',
-    AppID: '843296546878521345',
+    AppID: '839380589508558910',
 
     INTERACTIONS: {
         SLASH: true, // Should the interactions be enabled
-        GUILD_ID: "447069790150852609", // Guild ID where the interactions should be registered. [** Test you commands here first **]
+        // GUILD_ID: "447069790150852609", // Guild ID where the interactions should be registered. [** Test you commands here first **]
     },
 
     PRESENCE: {
