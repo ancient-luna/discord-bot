a489014b1403140b38c0229c42e316cc189ce12b
imsoondae <im.daeva@gmail.com>
2023-06-16 05:34:47 +0700

    help command and buttons

--- src/commands/message/entertainment/wisdomoflleud.js
+++ src/commands/message/entertainment/wisdomoflleud.js
@@ -1,11 +1,11 @@
-const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, AttachmentBuilder } = require("discord.js");
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 module.exports = new Object({
     name: "wisdomoflleud",
     description: "wisdomoflleud.",
     category: "Entertainment",
     cooldown: 0,
     usage: "",
-    aliases: ['help', 'about'],
+    aliases: ['help', 'about', 'test'],
     examples: [''],
     sub_commands: [],
     args: false,
@@ -22,57 +22,41 @@ module.exports = new Object({
      * @param {String[]} args
      */
     async execute(client, message, args) {
-        const IMGwisdom = new AttachmentBuilder("src/assets/ancientluna.png")
+
+        const cB = `\`\`\``
 
         const descGeneral = new EmbedBuilder()
-            .setDescription(`**General**\n\n\`!anime\` : search for anime through myanimelist\n\`!dictionary\` : search by word through urban dictionary\n\`!fellowcard\` : an exclusive discord member card\n\`!inspire\` : give you an inspiring quotes\n\`!instagram\` : show instagram profile\n\`!lifeadvice\` : give you an advice\n\`!reminder\` : set timer for your activity\n\`!translate\` : translator. support 14 languages\n\`!wikipedia\` : search by word through wikipedia\n\`!youtube\` : watch youtube together`)
-            .setImage("https://i.imgur.com/V0UhnF9.png")
-            .setColor("2b2d31")
-
-        const descToram = new EmbedBuilder()
-            .setDescription(`**Toram Online**\n\n\`!alchemy\` : give alchemy level list\n\`!bag\` : give bags materials for upgrading slot list\n\`!blacksmith\` : give blacksmith level list\n\`!crystalist\` : give crysta within their upgradeable crysta list\n\`!dye\` : give monthly dye list *(link)*\n\`!element\` : give information about element\n\`!fillstat\` : give fill stats weapon/armor calculator *(link)*\n\`!food\` : give information about food + *(link)*\n\`!level\` : give information about leveling list\n\`!map\` : give latest update about map\n\`!material\` : give information about materials spot\n\`!pet\` : give information about pets *(link)*\n\`!stat\` : give character stat calculator *(link)*`)
-            .setImage("https://i.imgur.com/V0UhnF9.png")
-            .setColor("2b2d31")
-
-        const descMinecraft = new EmbedBuilder()
-            .setDescription(`**Minecraft**\n\n\`!mcserver\` : show luna craft server information\n\`!mcuser\` : show detail of mc players`)
-            .setImage("https://i.imgur.com/V0UhnF9.png")
-            .setColor("2b2d31")
-
-        const descDEADFRONTIER = new EmbedBuilder()
-            .setDescription(`**Dead Frontier**\n\n\`!record\` : give latest information about your best record in game\n\`!status\` : give you latest game character information`)
-            .setImage("https://i.imgur.com/V0UhnF9.png")
-            .setColor("2b2d31")
-
-        const descBonus = new EmbedBuilder()
-            .setDescription(`**What is this?**\n\n\`!rawr\` : what does it do?\n\`!tomato\` : what does it do either?\n\`!spit\` : what does it do either after?`)
-            .setImage("https://i.imgur.com/V0UhnF9.png")
-            .setFooter({ text: "discord.gg/ancientluna", iconURL: 'https://i.imgur.com/1ooFs6C.gif' })
+            .setTitle(`About Ancient Luna Bot`)
+            .setDescription(`우리는 마치 달을 만난 것처럼 달렸다\nI'm a relic that born by [@imsoondae](https://www.instagram.com/imsoondae_/) to seek wisdom\nBlessed by [@biglebomb](https://discordapp.com/users/306545868054593537) to be alive while until now`)
+            .addFields(
+                { name: `Prefix`, value: `${cB}!${cB}`, inline: true },
+                { name: `Help (Prefix)`, value: `${cB}!help${cB}`, inline: true },
+                { name: `Help (Mention)`, value: `${cB}@AncientLuna${cB}`, inline: true },
+            )
             .setColor("2b2d31")
 
         let serverButton = new ActionRowBuilder()
             .addComponents(
                 new ButtonBuilder()
+                .setCustomId("btn-helpcmd")
+                .setLabel("Commands")
+                .setStyle(ButtonStyle.Success)
+            )
+            .addComponents(
+                new ButtonBuilder()
                     .setStyle(ButtonStyle.Link)
-                    .setLabel('Discord Server')
+                    .setLabel('Server')
                     .setURL('https://discord.com/invite/Sbp2nt8QHe')
             )
             .addComponents(
                 new ButtonBuilder()
                     .setStyle(ButtonStyle.Link)
-                    .setLabel('YouTube')
-                    .setURL('https://www.youtube.com/watch?v=wq0DURi1ekY')
-            )
-            .addComponents(
-                new ButtonBuilder()
-                    .setStyle(ButtonStyle.Link)
-                    .setLabel('Website')
-                    .setURL('https://ancientluna.org')
+                    .setLabel('Support')
+                    .setURL('https://ko-fi.com/daekid')
             )
 
         message.reply({
-            files: [IMGwisdom],
-            embeds: [descGeneral, descMinecraft, descToram, descDEADFRONTIER, descBonus],
+            embeds: [descGeneral],
             components: [serverButton]
         })
     }
