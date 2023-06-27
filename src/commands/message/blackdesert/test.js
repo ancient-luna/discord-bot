b87ac669e52f6293edfaa2f61606c1178041b339
imsoondae <im.daeva@gmail.com>
2023-06-27 11:03:40 +0700

    guild vacation

---
+++ src/commands/message/blackdesert/test.js
@@ -0,0 +1,43 @@
+
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+module.exports = new Object({
+    name: "embed",
+    description: "embed.",
+    category: "Blackdesert",
+    usage: "",
+    cooldown: 0,
+    aliases: [''],
+    examples: [''],
+    sub_commands: [],
+    args: false,
+    permissions: {
+        client: [],
+        user: ['ManageMessages'],
+        dev: false,
+    },
+    player: { voice: false, active: false, dj: false, },
+    /**
+     * 
+     * @param {import("../../../index")} client 
+     * @param {import("discord.js").Message} message
+     * @param {String[]} args
+     */
+    async execute(client, message, args) {
+
+        const btnVac = new ActionRowBuilder().addComponents(
+          new ButtonBuilder()
+            .setCustomId("btn-guildvacation")
+            .setLabel("Apply for Vacation")
+            .setStyle(ButtonStyle.Danger)
+        );
+
+        const embed = new EmbedBuilder()
+            .setDescription("Need to take a break from the game? **Click on the button to send a vacation letter**")
+            .setColor("2b2d31")
+
+        await message.channel.send({
+            embeds: [embed],
+            components: [btnVac]
+        }).catch(e => { });
+    }
+})
