36ba192c6105c99f0c2bf6bf1caecaf4c93d14a1
imsoondae <im.daeva@gmail.com>
2023-06-30 12:24:32 +0700

    completely done and well made

--- src/commands/message/entertainment/reminder.js
+++ src/commands/message/entertainment/reminder.js
@@ -25,17 +25,21 @@ module.exports = new Object({
     async execute(client, message, args) {
         if (!args[0]) return message.channel.send({ content: "Could you tell me the time? Ex: \`10m\` is 10 minutes." }).catch(e => { });
         if (!args[1]) return message.channel.send({ content: "And I need you to define a thing for the timer to remind you about also. Ex: \`We start running to meet the moon\`" }).catch(e => { });
-
-        let embed2 = new EmbedBuilder()
+        
+        const loadingTxt = await message.reply(`I keep it safe under the moon's name\nI will remind you back in **${args[0]}** <a:_util_loading:863317596551118858>`);
+        
+        let embedReminder = new EmbedBuilder()
             .setAuthor({ name: `${message.author.username}'s Reminder`, iconURL: message.author.displayAvatarURL() })
             .setDescription(`*" ${args.slice(1).join(" ")} "*`)
             .setColor('#2b2d31')
             .setFooter({ text: `Reminder was set for ${args[0]}` })
 
-        message.reply({ content: `I keep it safe under the moon's name\nI will remind you back in **${args[0]}** <a:_util_loading:863317596551118858>` }).catch(e => { })
-
         setTimeout(async () => {
-            message.channel.send({ content: `<:ancientluna_divinare:841754250949820416><@${message.author.id}>╮`, embeds: [embed2] }).catch(e => { })
+            message.channel.send({
+                content: `<:ancientluna_divinare:841754250949820416><@${message.author.id}>╮`,
+                embeds: [embedReminder]
+            }).catch(e => { })
+            loadingTxt.edit({ content: `Just successfully **reminded** you.` }).catch(e => { })
         }, ms(args[0]));
     }
 });
