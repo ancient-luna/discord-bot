737e9ff8d84749ada890b84e03db6ca8a2b8264f
imsoondae <im.daeva@gmail.com>
2023-07-04 09:07:08 +0700

    guild terms and apps

--- src/components/guildterms.js
+++ src/components/guildterms.js
@@ -19,9 +19,9 @@ module.exports = {
     .setAuthor({ name: "Ancient Luna Guild Terms", iconURL: "https://i.imgur.com/SOCuup9.png" })
     .setDescription("Welcome to the sanctuary of lights\nA home for the light seekers!")
     .addFields(
-        { name: `**Street Fight / PVP**`, value: `<:_1:1075437107704778943> Killing is OK __**only** in arsha__\n<:_2:1075437110183604355> Avoid bad manner and dont ever trash talking in any servers\n<:_3:1075437114721837157> For spots \`normal server\` can kill **but** ask for DFS first \`arsha server\` free kill`, inline: false },
+        { name: `**Street Fight / PVP**`, value: `<:_1:1075437107704778943> Killing is OK __**only** in arsha__\n<:_2:1075437110183604355> Avoid bad manner and dont ever trash talking in any servers\n<:_3:1075437114721837157> For spots \`normal server\` can kill **but** ask for DFS first \`arsha server\` free kill\n<:_4:1075437119050354770> Any toxicity behaviors in the guild and outside guild are strongly prohibited.  Action will be taken if it happens.`, inline: false },
         { name: `**Guild Quests**`, value: `<:_1:1075437107704778943> Guild Quests only can be taken for (Large) size only\n<:_2:1075437110183604355> SMH and Combat GQs can be taken around time 00.00 - 00.00 GMT+8\n<:_3:1075437114721837157> Life GQs can be taken around time 19.00 - 00.00 GMT+8`, inline: false },
-        { name: `**Vacation / Day-Off**`, value: `If you are unable to login for 7 days straight it is a must to let us know so we may not kick you out from the guild.\n> Apply for vacation in <#1123093540940029972>`, inline: false }
+        { name: `**Vacation / Day-Off**`, value: `If you are unable to login for 7 days straight it is a must to let us know so we may not kick you out from the guild. Apply for vacation in <#1123093540940029972>`, inline: false }
     )
     .setColor("2b2d31")
     .setTimestamp()
