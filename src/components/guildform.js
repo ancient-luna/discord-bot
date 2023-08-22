7c4b459b554a2c29902ea0aa80717e038e5545ee
imsoondae <im.daeva@gmail.com>
2023-08-23 01:07:32 +0700

    updated with timestamp

--- src/components/guildform.js
+++ src/components/guildform.js
@@ -95,6 +95,7 @@ module.exports = {
                     { name: `Guild Expectation`, value: `> ${expectGuild}`, inline: false },
                 )
                 .setColor('2b2d31')
+                .setTimestamp()
             modalInteraction.reply({ embeds: [embedForm] })
         })
         .catch((e)=> { console.log(e) })
