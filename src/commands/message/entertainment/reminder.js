1c529324b2c672589ab9188733882fea52599342
imsoondae <im.daeva@gmail.com>
2023-09-02 07:00:01 +0700

    adding time counter

--- src/commands/message/entertainment/reminder.js
+++ src/commands/message/entertainment/reminder.js
@@ -23,16 +23,22 @@ module.exports = new Object({
      * @param {String[]} args
      */
     async execute(client, message, args) {
-        if (!args[0]) return message.channel.send({ content: "Could you tell me the time? Ex: \`10m\` is 10 minutes." }).catch(e => { });
-        if (!args[1]) return message.channel.send({ content: "And I need you to define a thing for the timer to remind you about also. Ex: \`We start running to meet the moon\`" }).catch(e => { });
+
+        const timeReminder = args[0]
+        const reminderMessage = args[1];
+
+        const timeCounter = Date.now() + ms(timeReminder);
+
+        if (!timeReminder) return message.channel.send({ content: "Could you tell me the time? Ex: \`10m\` is 10 minutes." }).catch(e => { });
+        if (!reminderMessage) return message.channel.send({ content: "And I need you to define a thing for the timer to remind you about also. Ex: \`We start running to meet the moon\`" }).catch(e => { });
         
-        const loadingTxt = await message.reply(`I keep it safe under the moon's name\nI will remind you back in **${args[0]}** <a:_util_loading:863317596551118858>`);
+        const loadingTxt = await message.reply(`I keep it safe under the moon's name\nI will remind you back <t:${Math.floor(timeCounter/1000)}:R> <a:_util_loading:863317596551118858>`);
         
         let embedReminder = new EmbedBuilder()
             .setAuthor({ name: `${message.member.displayName}'s Reminder`, iconURL: message.author.displayAvatarURL() })
             .setDescription(`*" ${args.slice(1).join(" ")} "*`)
             .setColor('#2b2d31')
-            .setFooter({ text: `Reminder was set for ${args[0]}` })
+            .setFooter({ text: `Reminder was set for ${timeReminder}` })
 
         setTimeout(async () => {
             message.channel.send({
@@ -40,7 +46,7 @@ module.exports = new Object({
                 embeds: [embedReminder]
             }).catch(e => { })
             loadingTxt.edit({ content: `Just successfully **reminded** you.` }).catch(e => { })
-        }, ms(args[0]));
+        }, ms(timeReminder));
     }
 });
 
