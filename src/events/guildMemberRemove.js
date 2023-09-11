ac6099bdf83afef9534e5377778d4715a3a8b816
imsoondae <im.daeva@gmail.com>
2023-09-11 17:39:29 +0700

    displayName

--- src/events/guildMemberRemove.js
+++ src/events/guildMemberRemove.js
@@ -12,7 +12,7 @@ module.exports = new Object({
         const { guild } = member;
         const channel = guild.channels.cache.get(client.config.gatewayChannel);
         const leavingText = new EmbedBuilder()
-            .setDescription(`The lights get dimmed! **${member.member.displayName}** leaving the sanctuary`)
+            .setDescription(`The lights get dimmed! **${member.displayName}** leaving the sanctuary`)
             .setFooter({ text: `ID: ${member.user.id} (${member.user.username})`, iconURL: member.user.displayAvatarURL({ dynamic: true, size: 512 }) })
             .setColor('Red');
         return channel.send({ embeds: [leavingText] })
