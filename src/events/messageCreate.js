f6490889b6ae6d60c790983c529abca24d8186b6
imsoondae <im.daeva@gmail.com>
2023-09-18 12:39:17 +0700

    lil fix oops

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -81,9 +81,7 @@ module.exports = new Object({
       const embedLucent = new EmbedBuilder()
         .setTitle(`Ancient Luna Guild: The Lucent Fountain <:ancientluna_pure_luna:866781517312688178>`)
         .setDescription(`𝔗𝔥𝔢 𝔪𝔬𝔬𝔫 𝔦𝔰 𝔞 𝔩𝔬𝔶𝔞𝔩 𝔠𝔬𝔪𝔭𝔞𝔫𝔦𝔬𝔫. 𝔗𝔥𝔢 𝔪𝔬𝔬𝔫 𝔲𝔫𝔡𝔢𝔯𝔰𝔱𝔞𝔫𝔡𝔰 𝔴𝔥𝔞𝔱 𝔦𝔱 𝔪𝔢𝔞𝔫𝔰 𝔱𝔬 𝔟𝔢 𝔥𝔲𝔪𝔞𝔫.`)
-        // .setFooter({ text: `this bot won't read any messages of your replies` })
         .setColor("2b2d31")
-        // .setThumbnail(`${lunaThumbnail[Math.floor(Math.random() * lunaThumbnail.length)]}`)
         const btnLucent = new ActionRowBuilder()
         .addComponents(
             new ButtonBuilder()
