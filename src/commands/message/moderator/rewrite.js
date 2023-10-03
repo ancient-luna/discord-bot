bf204418b94271b59151f0cfa31c43ebc8177194
imsoondae <im.daeva@gmail.com>
2023-10-03 22:12:55 +0700

    footers

--- src/commands/message/moderator/rewrite.js
+++ src/commands/message/moderator/rewrite.js
@@ -37,6 +37,7 @@ module.exports = new Object({
             const editEmbed = new EmbedBuilder()
                 .setDescription(editQuery)
                 .setColor(`2b2d31`)
+                .setFooter({ text: `*roles still claimable on closed game`})
 
             // const editButton = new ActionRowBuilder()
             // .addComponents(
