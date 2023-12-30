aa22ae497807a1ceb0a7fb92b3b71240354dd5e7
imsoondae <im.daeva@gmail.com>
2023-12-30 09:48:57 +0700

    much better :)

--- src/commands/message/deadfrontier/status.js
+++ src/commands/message/deadfrontier/status.js
@@ -6,7 +6,7 @@ module.exports = new Object({
     category: "Deadfrontier",
     usage: "",
     cooldown: 0,
-    aliases: ['teststat'],
+    aliases: [''],
     examples: [''],
     sub_commands: [],
     args: false,
