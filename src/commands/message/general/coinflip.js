c3fe5cfffcf5501d138d5f24220e0b02098205d2
imsoondae <im.daeva@gmail.com>
2024-02-17 22:13:57 +0700

    coinflip

---
+++ src/commands/message/general/coinflip.js
@@ -0,0 +1,47 @@
+const { EmbedBuilder, AttachmentBuilder } = require('discord.js');
+
+module.exports = new Object({
+    name: "coinflip",
+    description: "coinflip.",
+    category: "Entertainment",
+    usage: "",
+    cooldown: 0,
+    aliases: [''],
+    examples: [''],
+    sub_commands: [],
+    args: false,
+    permissions: {
+        client: [],
+        user: [],
+        dev: false,
+    },
+    player: { voice: false, active: false, dj: false, },
+    /**
+     * 
+     * @param {import("../../../index")} client 
+     * @param {import("discord.js").Message} message
+     * @param {String[]} args
+     */
+    async execute(client, message, args) {
+
+        const head = new EmbedBuilder()
+            .setTitle('ℭ𝔬𝔦𝔫 𝔉𝔩𝔦𝔭: ℌ𝔢𝔞𝔡')
+            .setDescription(`${message.member.displayName} flipped the coin and result is **Head**`)
+            .setColor('2b2d31')
+            .setThumbnail('https://i.imgur.com/X61MBiD.png')
+            .setTimestamp()
+
+        const tail = new EmbedBuilder()
+            .setTitle('ℭ𝔬𝔦𝔫 𝔉𝔩𝔦𝔭: 𝔗𝔞𝔦𝔩')
+            .setDescription(`${message.member.displayName} flipped the coin and result is **Tail**`)
+            .setColor('2b2d31')
+            .setThumbnail('https://i.imgur.com/nlYa0I3.png')
+            .setTimestamp()
+
+        const coin = [head, tail];
+
+        const flipped = coin[Math.floor(Math.random() * coin.length)];
+
+        message.channel.send({ embeds: [flipped] });
+    }
+});
