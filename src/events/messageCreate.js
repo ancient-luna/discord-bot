0517454b2412d46222e78f5104757ecdefa26ca1
imsoondae <im.daeva@gmail.com>
2024-03-10 19:55:27 +0700

    WOW!

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -184,7 +184,7 @@ module.exports = new Object({
 
         const aiChat = {
           method: 'GET',
-          url: 'https://google-bard1.p.rapidapi.com/v3/chat/gemini-1.0-pro-001',
+          url: 'https://google-bard1.p.rapidapi.com/v3/chat/gemini-1.0-pro',
           headers: {
             'api-key': process.env.GOOGLE_MAKERSUITE_KEY,
             message: message.content,
