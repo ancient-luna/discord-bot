9ecf3f362fa8ae4eb11dc271071fde669f10b0a3
imsoondae <im.daeva@gmail.com>
2024-03-27 10:43:42 +0700

    adding song link

--- src/commands/message/general/spotify.js
+++ src/commands/message/general/spotify.js
@@ -1,4 +1,4 @@
-const { EmbedBuilder, AttachmentBuilder } = require('discord.js');
+const { EmbedBuilder, AttachmentBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const canvacord = require('canvacord');
 
 module.exports = new Object({
@@ -7,7 +7,7 @@ module.exports = new Object({
     category: "Entertainment",
     usage: "",
     cooldown: 0,
-    aliases: [''],
+    aliases: ['test'],
     examples: [''],
     sub_commands: [],
     args: false,
@@ -27,8 +27,6 @@ module.exports = new Object({
 
         let target = message.mentions.members.first();
 
-        // if (target.bot) return await message.reply({ content: "Well, it is a BOT. You cant track them" })
-
         if (!target) {
             message.react("❓").catch((e) => { });
             return message.reply({ content: "How can I track when theres none. Mention one" }).then((msg) => {
@@ -41,39 +39,49 @@ module.exports = new Object({
         else if (target.presence.activities.length > 1) status = target.presence.activities[1];
 
         if (target.presence.activities.length === 0 || status.name !== "Spotify" && status.type !== "LISTENING") {
-            return await message.reply({ content: `${target.displayName} is not listening spotify` })
+            return await message.reply({ content: `${target.displayName} is not listening to Spotify` });
         }
 
         if (status !== null && status.name === "Spotify" && status.assets !== null) {
-            let image = `https://i.scdn.co/image/${status.assets.largeImage.slice(8)}`,
-            name = status.details,
-            artist = status.state,
-            album = status.assets.largeText
+            let image = `https://i.scdn.co/image/${status.assets.largeImage.slice(8)}`;
+            let name = status.details;
+            let artist = status.state;
+            let album = status.assets.largeText;
 
             const card = new canvacord.Spotify()
-            .setAuthor(artist)
-            .setAlbum(album)
-            .setStartTimestamp(status.timestamps.start)
-            .setEndTimestamp(status.timestamps.end)
-            .setImage(image)
-            .setTitle(name)
+                .setAuthor(artist)
+                .setAlbum(album)
+                .setStartTimestamp(status.timestamps.start)
+                .setEndTimestamp(status.timestamps.end)
+                .setImage(image)
+                .setTitle(name);
 
             const Card = await card.build();
-            const attachments = new AttachmentBuilder(Card, { name: "spotify.png" })
+            const attachments = new AttachmentBuilder(Card, { name: "spotify.png" });
 
             const embed = new EmbedBuilder()
-            .setTitle(`Currently listening to`)
-            .setImage(`attachment://spotify.png`)
-            .setTimestamp()
-            .setColor(`1db954`)
-            .setFooter({ text: `Spotify Tracker`, iconURL: `https://i.imgur.com/o0RHwu2.png` })
+                .setTitle(`Currently listening to`)
+                .setImage(`attachment://spotify.png`)
+                .setTimestamp()
+                .setColor(`1db954`)
+                .setFooter({ text: `Spotify`, iconURL: `https://i.imgur.com/o0RHwu2.png` });
 
-            // await message.react("🎧").catch((e) => { });
+            const trackId = status.syncId;
 
+            const link = new ActionRowBuilder()
+                    .addComponents(
+                        new ButtonBuilder()
+                            .setStyle(ButtonStyle.Link)
+                            .setLabel(`listen the song here`)
+                            .setURL(`https://open.spotify.com/track/${trackId}`)
+                    )
+
+            // Sending the message with embed and button
             await message.reply({
                 embeds: [embed],
-                files: [attachments]
-            })
+                files: [attachments],
+                components: [link]
+            });
         }
     }
 });
