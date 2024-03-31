9b8699c9b9e3a60853d6609e5568a2b853b6e5c2
imsoondae <im.daeva@gmail.com>
2024-03-31 20:31:40 +0700

    much better

--- src/commands/message/general/seeker.js
+++ src/commands/message/general/seeker.js
@@ -29,48 +29,50 @@ module.exports = new Object({
         try {
             const member = message.mentions.members.first() || message.member;
             const fetchedMembers = await message.guild.members.fetch();
-        
+
             const profileBuffer = await profileImage(member.id);
             const imageAttachment = new AttachmentBuilder(profileBuffer, { name: 'profile.png' });
-        
+
             const joinPosition = Array.from(fetchedMembers
                 .sort((a, b) => a.joinedTimestamp - b.joinedTimestamp)
                 .keys())
                 .indexOf(member.id) + 1;
-        
+
+            const everyoneRole = message.guild.roles.cache.find(role => role.name === '@everyone');
             const topRoles = member.roles.cache
+                .filter(role => role !== everyoneRole)
                 .sort((a, b) => b.position - a.position)
                 .map(role => role)
-                // .slice(0, 3);
-        
+            // .slice(0, 3);
+
             // const userBadges = member.user.flags.toArray();
-        
+
             const joinTime = parseInt(member.joinedTimestamp / 1000);
             const createdTime = parseInt(member.user.createdTimestamp / 1000);
-        
+
             const Booster = member.premiumSince ? "<a:_ab_discord_boost_spin:965778537334312970> active" : "none";
-        
+
             const avatarButton = new ButtonBuilder()
                 .setLabel('Avatar')
                 .setStyle(5)
                 .setURL(member.displayAvatarURL());
-        
+
             const bannerButton = new ButtonBuilder()
                 .setLabel('Banner')
                 .setStyle(5)
                 .setURL((await member.user.fetch()).bannerURL() || 'https://example.com/default-banner.jpg');
-        
+
             const row = new ActionRowBuilder()
                 .addComponents(avatarButton, bannerButton);
-        
+
             const Embed = new EmbedBuilder()
-                .setAuthor({ name: `${member.displayName}'s General Information`, iconURL: member.displayAvatarURL() })
+                .setAuthor({ name: `User Profile Information` })
                 .setColor('Aqua')
-                .setDescription(`On <t:${joinTime}:D>, <@${member.id}> joined as the **${addSuffix(joinPosition)}** member of this server.`)
+                .setDescription(`<@${member.id}> joined as the **${addSuffix(joinPosition)}** member of this server.`)
                 .setImage("attachment://profile.png")
                 .addFields([
                     { name: "Account Created", value: `<t:${createdTime}:R>`, inline: true },
-                    { name: "Joined Since", value: `<t:${joinTime}:R>`, inline: true },
+                    { name: "Joined Since", value: `<t:${joinTime}:D>`, inline: true },
                     // { name: "User ID", value: `${member.id}`, inline: true },
                     // { name: "Badges", value: `${addBadges(userBadges).join("")}`, inline: true },
                     { name: "Booster", value: `${Booster}`, inline: true },
@@ -78,9 +80,9 @@ module.exports = new Object({
                 ])
                 .setColor(client.config.embedColorTrans)
                 .setFooter({ text: `ID: ${member.id}` })
-        
+
             loadingTxt.edit({ content: '⁣', embeds: [Embed], components: [row], files: [imageAttachment] });
-        
+
         } catch (error) {
             loadingTxt.edit({ content: "An error in the code" });
             throw error;
@@ -90,12 +92,12 @@ module.exports = new Object({
 
 function addSuffix(number) {
     if (number % 100 >= 11 && number % 100 <= 13)
-      return number + "th";
-  
+        return number + "th";
+
     switch (number % 10) {
-      case 1: return number + "st";
-      case 2: return number + "nd";
-      case 3: return number + "rd";
+        case 1: return number + "st";
+        case 2: return number + "nd";
+        case 3: return number + "rd";
     }
     return number + "th";
 }
