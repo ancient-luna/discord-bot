9d62d254987fa4deffbdf43f3c60442f733f347a
imsoondae <im.daeva@gmail.com>
2024-04-14 13:02:37 +0700

    spitters

--- src/commands/message/fun/spit.js
+++ src/commands/message/fun/spit.js
@@ -33,7 +33,7 @@ module.exports = new Object({
         }
         await message.react("💢").catch((e) => { });
         await message.channel.send({
-            content: `***Puih!!*** <@643945264868098049> spits on <@${target.user.id}> 💦`,
+            content: `***Puih!!*** 💦 <@643945264868098049> spits on <@${target.user.id}>\n_ _`,
             files: [IMGpuih]
         }).catch((e) => { });
     }
