1ad889931beb3642af0055ba07f22dabf34dce8a
imsoondae <im.daeva@gmail.com>
2024-06-21 23:11:03 +0700

    best console+

--- src/utils/Console.js
+++ src/utils/Console.js
@@ -11,7 +11,7 @@ module.exports = class Console {
             case "event": { return console.log(`• [ Events ] ` + "   => " + content) }
             case "client": { return console.log(`• [ Client ] ` + "   => " + content) }
             case "api": { return console.log(`• [ Api ] ` + "      => " + content) }
-            default: throw new TypeError("Logger type must be either warn, debug, log, ready, cmd or error.");
+            default: throw new TypeError("Logger type must be either warn, debug, log, ready, cmd or error.")
         }
     }
 };
