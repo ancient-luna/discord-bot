748593865789f8eab48e43e6b0139db757ca00d2
imsoondae <im.daeva@gmail.com>
2024-08-09 16:17:23 +0700

    vac

--- src/commands/message/blackdesert/undisciple.js
+++ src/commands/message/blackdesert/undisciple.js
@@ -29,18 +29,20 @@ module.exports = new Object({
 
         let role = '1060982357538119850';
 
-        // const decide = "\n\n> We always welcome you back to the guild when decide to be active again";
+        let isVacation = args[0] && args[0].toLowerCase() === "vac";
+
+        const letter = "Your **Lunar Disciples** role has been removed and no longer have access to guild only channels in **[Ecplise Boarder Hall](https://discord.com/channels/447069790150852609/1060992670035619931)** category. If have any questions regarding this don't hesitate to reach and mention the Ancestor and the Elders in **[#city](https://discord.com/channels/447069790150852609/1049228301807407156)**";
 
         const addLD = new EmbedBuilder()
             .setAuthor({ name: "ROLE REMOVED", iconURL: "https://i.imgur.com/hSlBkoj.png" })
-            .setDescription("Your **Lunar Disciples** role has been removed and no longer have access to guild only channels in **[Ecplise Boarder Hall](https://discord.com/channels/447069790150852609/1060992670035619931)** category. If have any questions regarding this dont hesitate to reach and mention the Ancestor and the Elders in **[#city](https://discord.com/channels/447069790150852609/1049228301807407156)**")
+            .setDescription( isVacation ? `${letter}\n\n-# You have been kicked due to inactivity without vacation. We will welcome you back if you decide to play actively again and to rejoin the guild ♡ **missing you**` : letter )
             .setTimestamp()
             .setColor(client.config.embedColorTrans)
-            .setFooter({ text: "Ancient Luna Guild: We ran as if to meet the moon" })
+            .setFooter({ text: "Ancient Luna Guild: We ran as if to meet the moon" });
 
         await target.user.send({ embeds: [addLD] }).then(target.roles.remove(role)).catch((e) => { });
 
-        await message.react("✅").then(setTimeout(() => message.delete().catch((e) => { }), 5000)).catch((err) => { throw err; })
+        await message.react("✅").then(setTimeout(() => message.delete().catch((e) => { }), 5000)).catch((err) => { throw err; });
     }
 })
 
