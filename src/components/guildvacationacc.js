3e26e715ab30a17620cfa0eaa0ff3aabd81d2bcb
imsoondae <im.daeva@gmail.com>
2024-08-22 02:08:40 +0700

    adding days

--- src/components/guildvacationacc.js
+++ src/components/guildvacationacc.js
@@ -17,9 +17,11 @@ module.exports = {
 
     const originalMessage = await interaction.message.fetch(); 
     const userIdMatch = originalMessage.embeds[0]?.description?.match(/\*\*\*\<\@(\d+)\>\*\*\*/);
-    const userID = userIdMatch ? userIdMatch[1] : "`???`";
+    const dayMatch = originalMessage.embeds[0]?.description?.match(/Requested Day Off: (\d+) Days/);
+    const userID = userIdMatch ? userIdMatch[1] : "`undefined`";
+    const dayOff = dayMatch ? dayMatch[1] : "`undefined`";
 
-    interaction.guild.channels.cache.get('1060992670035619931').send({ content: `**Good news** to <@${userID}> ! Your vacation has been **approved** by <@${interaction.user.id}> <:vcon_vote_accepted:859075138329903114>` });
+    interaction.guild.channels.cache.get('1060992670035619931').send({ content: `**Good news to <@${userID}> !** Your ${dayOff} days off for vacation has been **approved** by <@${interaction.user.id}> <:vcon_vote_accepted:859075138329903114>` });
         
     const updatedButton = new ButtonBuilder()
       .setCustomId('btn-guildvacationacc')
