41494d0d3ed721f345b5e4ab9054775cc3e12bb4
imsoondae <im.daeva@gmail.com>
2024-10-31 02:12:57 +0700

    fellowcard

--- src/commands/message/moderator/lightseeker.js
+++ src/commands/message/moderator/lightseeker.js
@@ -33,6 +33,12 @@ module.exports = new Object({
                     .setLabel("Get more roles here")
                     .setURL("https://discord.com/channels/447069790150852609/864556584818835456")
             )
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId("btn-fellowcard")
+                    .setLabel("Your fellowcard")
+                    .setStyle(ButtonStyle.Primary)
+            )
         message.guild.channels.cache.get('452842830776369152').send({
             content: `Welcome <@${target.user.id}>, to **the sanctuary of light**. The <@&843523544620335124> welcome you as one of true light seekers <:ancientluna_pure_luna:866781517312688178>\n-# ${target.user.displayName} has passed the trial by understand our wisdom of lleud to reach this warm sanctuary deeper`,
             components: [welcomeButton]
