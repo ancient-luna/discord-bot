d9b502af0db0e2f465d5ded2dda520ebb9a586e0
imsoondae <im.daeva@gmail.com>
2024-11-07 11:06:14 +0700

    a relic that born to seek a wisdom

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -164,7 +164,7 @@ module.exports = new Object({
           chatModel: "gpt-4o",
           readFiles: true,
           readImages: true,
-          instruction: 'Your name is Luna, a diety and a wisdom keeper of Ancient Luna sanctuary for the ancestor',
+          instruction: 'Your name is Luna. You were born as a relic and a wisdom keeper of Ancient Luna sanctuary by the ancestor',
           memory: {
             memoryOn: true,
             id: message.author.id
