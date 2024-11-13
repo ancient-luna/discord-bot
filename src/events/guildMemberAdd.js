e4836c408feb4614a3c239be8edd28785b552dd9
imsoondae <im.daeva@gmail.com>
2024-11-13 18:27:33 +0700

    duh

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -17,7 +17,7 @@ module.exports = new Object({
             .setAvatarBorder('#7a97aa')
             .setTitle(member.user.username,'#7a97aa')
             .setDescription('we ran as if to meet the moon')
-            .setBackground('image','https://ik.imagekit.io/al/wlcmcrd.png')
+            .setBackground('image','https://ik.imagekit.io/al/welcomecard.png')
             .build();
         const cardBuffer = Buffer.from(card);
         const attachment = new AttachmentBuilder(cardBuffer, { name: `${member.user.id}.png` });
