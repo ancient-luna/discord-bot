93fd33099f2b058b9a43fac8e1c4d97c4b9df7cf
imsoondae <im.daeva@gmail.com>
2025-05-08 01:09:43 +0700

    fixed embed

--- src/components/tagguildlink.js
+++ src/components/tagguildlink.js
@@ -15,7 +15,7 @@ module.exports = {
    */
   execute: async (client, interaction) => {
     return interaction.reply({
-      content: '*https://discord.gg/XJCtfTPBfu*',
+      content: 'https://discord.gg/XJCtfTPBfu',
       ephemeral: true,
     });
   },
