36858b47e8ea45897265a2c369ab8ea619114a40
imsoondae <im.daeva@gmail.com>
2025-05-08 11:19:56 +0700

    howtotag

---
+++ src/components/tagguildmobile.js
@@ -0,0 +1,38 @@
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+const { stripIndent } = require("common-tags");
+
+module.exports = {
+  name: "tagguildmobile",
+  id: "btn-tagguildmobile",
+  permissions: {
+    client: [],
+    user: [],
+    dev: false,
+  },
+  /**
+   * @param {import("../index")} client
+   * @param {import("discord.js").ButtonInteraction} interaction
+   */
+  execute: async (client, interaction) => {
+    const howTo1 = new EmbedBuilder()
+      .setTitle("Ⅰ. Profile")
+      .setImage("https://i.imgur.com/aBppeYU.jpeg")
+      .setColor("#3c3d4f")
+
+    const howTo2 = new EmbedBuilder()
+      .setTitle("Ⅱ. Edit Profile")
+      .setImage("https://i.imgur.com/HLd6qzB.jpeg")
+      .setColor("#3c3d4f")
+
+    const howTo3 = new EmbedBuilder()
+      .setTitle("Ⅲ. Server Tag")
+      .setImage("https://i.imgur.com/eG2Vxf9.jpeg")
+      .setFooter('*scroll down until you see the server tag option')
+      .setColor("#3c3d4f")
+    
+    return interaction.reply({
+      content: 'https://discord.gg/XJCtfTPBfu',
+      ephemeral: true,
+    });
+  },
+};
---
+++ src/components/tagguildpc.js
@@ -0,0 +1,38 @@
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+const { stripIndent } = require("common-tags");
+
+module.exports = {
+  name: "tagguildpc",
+  id: "btn-tagguildpc",
+  permissions: {
+    client: [],
+    user: [],
+    dev: false,
+  },
+  /**
+   * @param {import("../index")} client
+   * @param {import("discord.js").ButtonInteraction} interaction
+   */
+  execute: async (client, interaction) => {
+    const howTo1 = new EmbedBuilder()
+      .setTitle("Ⅰ. User Settings")
+      .setImage("https://i.imgur.com/Wgo5BJy.png")
+      .setColor("#3c3d4f")
+
+    const howTo2 = new EmbedBuilder()
+      .setTitle("Ⅱ. Profiles")
+      .setImage("https://i.imgur.com/2r3AMGs.png")
+      .setColor("#3c3d4f")
+
+    const howTo3 = new EmbedBuilder()
+      .setTitle("Ⅲ. Server Tag")
+      .setImage("https://i.imgur.com/IvK0tA5.png")
+      .setFooter('*scroll down until you see the server tag option')
+      .setColor("#3c3d4f")
+      
+    return interaction.reply({
+      embeds: [howTo1, howTo2, howTo3],
+      ephemeral: true,
+    });
+  },
+};
