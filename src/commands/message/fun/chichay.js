c1413d7a49716f4fe365f60b63bf6eb878653f9f
imsoondae <im.daeva@gmail.com>
2025-05-16 20:25:02 +0700

    chichay

---
+++ src/commands/message/fun/chichay.js
@@ -0,0 +1,32 @@
+const { AttachmentBuilder } = require("discord.js");
+module.exports = new Object({
+    name: "chichay",
+    description: "uiiaiouiiiai",
+    category: "Fun",
+    usage: "",
+    cooldown: 0,
+    aliases: ['uiia'],
+    examples: [''],
+    sub_commands: [],
+    args: false,
+    permissions: {
+        client: [],
+        user: [],
+        dev: false,
+    },
+    player: { voice: false, active: false, dj: false, },
+    /**
+     * 
+     * @param {import("../../../index")} client 
+     * @param {import("discord.js").Message} message
+     * @param {String[]} args
+     */
+    async execute(client, message, args) {
+        let chichay = new AttachmentBuilder("src/assets/react/chichay.gif")
+        let uia = new AttachmentBuilder("src/assets/react/u ii a ia chichay uu iai.mp3")
+        await message.channel.send({
+            content: "⁣",
+            files: [chichay, uia]
+        });
+    }
+});
