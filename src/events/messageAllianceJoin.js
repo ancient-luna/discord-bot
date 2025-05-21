4e14e0bc294ab8e82d49f56cf72cb910db58a50f
imsoondae <im.daeva@gmail.com>
2025-05-21 19:57:44 +0700

    there.

--- src/events/messageAllianceJoin.js
+++ src/events/messageAllianceJoin.js
@@ -15,12 +15,11 @@ module.exports = {
     const avatarUrl = member.user.displayAvatarURL({ extension: "png", dynamic: true, size: 512 });
 
     try {
-      const sent = await webhookClient.send({
+      await webhookClient.send({
         content: '-# <:ico_join:1374730865430495232> joined the alliance server ⓘ [TacoYaki](<https://discord.gg/tjdhmd38P5>)',
         username: `${displayName}`,
         avatarURL: avatarUrl,
       });
-      await client.db.set(`mirror_${message.id}`, sent.id);
     } catch (err) {
       console.error("failed to forward message:", err);
     }
