bc307a0afef95182db76ebf7d36a569110c96e90
imsoondae <im.daeva@gmail.com>
2025-05-27 06:33:20 +0700

    read from root-subfolds

--- src/scripts/Button.js
+++ src/scripts/Button.js
@@ -1,15 +1,29 @@
-const { readdirSync } = require("node:fs");
-const { join } = require("path");
+const { readdirSync, statSync } = require("node:fs");
+const { join, extname } = require("path");
+/**
+ * @param {string} dir
+ * @returns {string[]}
+ */
+function getJSFiles(dir) {
+  const files = [];
+  for (const file of readdirSync(dir)) {
+    const fullPath = join(dir, file);
+    if (statSync(fullPath).isDirectory()) {
+      files.push(...getJSFiles(fullPath));
+    } else if (extname(fullPath) === ".js") {
+      files.push(fullPath);
+    }
+  }
+  return files;
+}
 /**
  * @param {import('../index')} client
  */
 module.exports = (client) => {
   let count = 0;
-  const buttonFile = readdirSync(join(__dirname, "..", "components")).filter(
-    (files) => files.endsWith(".js")
-  );
-  for (const files of buttonFile) {
-    const buttons = require(`../components/${files}`);
+  const buttonFiles = getJSFiles(join(__dirname, "..", "components"));
+  for (const filePath of buttonFiles) {
+    const buttons = require(filePath);
     client.ButtonInt.set(buttons.id, buttons);
     count++;
   }
--- src/scripts/Events.js
+++ src/scripts/Events.js
@@ -1,17 +1,35 @@
-const { readdirSync } = require('node:fs');
-const { join } = require('path');
+const { readdirSync, statSync } = require("node:fs");
+const { join, extname } = require("path");
 /**
- * @param {import('../index')} client 
+ * @param {string} dir
+ * @returns {string[]}
+ */
+function getJSFiles(dir) {
+  const files = [];
+  for (const file of readdirSync(dir)) {
+    const fullPath = join(dir, file);
+    if (statSync(fullPath).isDirectory()) {
+      files.push(...getJSFiles(fullPath));
+    } else if (extname(fullPath) === ".js") {
+      files.push(fullPath);
+    }
+  }
+  return files;
+}
+/**
+ * @param {import('../index')} client
  */
 module.exports = (client) => {
-    let count = 0
-    const eventFiles = readdirSync(join(__dirname, "..", "events")).filter((files) => files.endsWith(".js"));
-    for (const files of eventFiles) {
-        const event = require(`../events/${files}`);
-        if (event.once) {
-            client.once(event.name, (...args) => event.execute(client, ...args));
-        } else { client.on(event.name, (...args) => event.execute(client, ...args)) };
-        count++
-    };
-    client.console.log(`Loaded: ${count}`, "event");
-}
+  let count = 0;
+  const eventFiles = getJSFiles(join(__dirname, "..", "events"));
+  for (const filePath of eventFiles) {
+    const event = require(filePath);
+    if (event.once) {
+      client.once(event.name, (...args) => event.execute(client, ...args));
+    } else {
+      client.on(event.name, (...args) => event.execute(client, ...args));
+    }
+    count++;
+  }
+  client.console.log(`Loaded: ${count}`, "event");
+};
