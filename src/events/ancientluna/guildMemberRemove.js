ce568da5b5526f005bd1c566e5df03c59c1172c5
imsoondae <im.daeva@gmail.com>
2025-06-22 08:42:47 +0700

    might be.

--- src/events/ancientluna/guildMemberRemove.js
+++ src/events/ancientluna/guildMemberRemove.js
@@ -9,8 +9,7 @@ module.exports = new Object({
     async execute(client, member) {
         if (member.partial) await member.user.fetch();
         if (!member.guild) return;
-        const { guild } = member;
-        const channel = guild.channels.cache.get(client.config.gatewayChannel);
+        const channel = member.guild.channels.cache.get(client.config.gatewayChannel);
         const leavingText = new EmbedBuilder()
             .setDescription(`The lights get dimmed! **${member.displayName}** leaving the sanctuary`)
             .setFooter({ text: `${member.user.id} (u) ${member.user.username}`, iconURL: member.user.displayAvatarURL({ dynamic: true, size: 512 }) })
