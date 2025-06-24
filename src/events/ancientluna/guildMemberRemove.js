3583afd0d844a234a130b81ebe514e3a974bd990
imsoondae <im.daeva@gmail.com>
2025-06-25 06:48:15 +0700

    declare

--- src/events/ancientluna/guildMemberRemove.js
+++ src/events/ancientluna/guildMemberRemove.js
@@ -7,14 +7,19 @@ module.exports = new Object({
      * @param {import('discord.js').GuildMember|import('discord.js').PartialGuildMember} member
      */
     async execute(client, member) {
-        if (member.partial) await member.user.fetch();
-        if (!member.guild) return;
-        const channel = member.guild.channels.cache.get(client.config.gatewayChannel);
-        const leavingText = new EmbedBuilder()
-            .setDescription(`The lights get dimmed! **${member.displayName}** leaving the sanctuary`)
-            .setFooter({ text: `${member.user.id} (u) ${member.user.username}`, iconURL: member.user.displayAvatarURL({ dynamic: true, size: 512 }) })
-            .setColor('Red')
-            .setTimestamp();
-        return channel.send({ embeds: [leavingText] })
+        try {
+            if (member.partial) await member.user.fetch();
+            if (!member.guild) return;
+            const channel = await member.guild.channels.fetch(client.config.gatewayChannel).catch(() => null);
+            if (!channel || !channel.send) return;
+            const leavingText = new EmbedBuilder()
+                .setDescription(`The lights get dimmed! **${member.displayName}** leaving the sanctuary`)
+                .setFooter({ text: `${member.user.id} (u) ${member.user.username}`, iconURL: member.user.displayAvatarURL({ dynamic: true, size: 512 }) })
+                .setColor('Red')
+                .setTimestamp();
+            return channel.send({ embeds: [leavingText] })
+        } catch (err) {
+            console.error("Error in guildMemberRemove:", err);
+        }
     }
 })
