dc0cf1b5fe33a77a6e3077a6d267f4737b0a6f53
imsoondae <im.daeva@gmail.com>
2025-08-19 16:00:07 +0700

    belongs to sanctuary

--- src/commands/message/moderator/sighting.js
+++ src/commands/message/moderator/sighting.js
@@ -24,12 +24,13 @@ module.exports = new Object({
     async execute(client, message, args) {
         const guild = message.guild;
         const serverId = guild.id;
+        if (serverId !== client.config.ancientLunaServer) return message.reply(`I think.. you've just lost, didn't you?\n-# this command belongs to the sanctuary`)
         const insighted = guild.members.cache.filter(member => !member.user.bot && member.roles.cache.filter(role => role.id !== serverId).size === 0);
         if (insighted.size > 0) {
             const mentionList = insighted.map(m => `<@${m.id}>`).join(", ");
             return message.reply({
                 content: `Found **${insighted.size}** members with no roles:\n${mentionList.length > 1500 ? 'Too many to list.' : mentionList}`,
-                allowedMentions: { parse: [] } // avoid mass mentions
+                allowedMentions: { parse: [] }
             });
         } else {
             return message.reply(`<:srv_accepted:1334885365676507188> Every seekers has at least one role.`);
