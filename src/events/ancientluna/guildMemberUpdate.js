adc34782d32839f529ef8254914276dcb8840759
imsoondae <im.daeva@gmail.com>
2025-11-25 03:47:12 +0700

    forgotten this one

--- src/events/ancientluna/guildMemberUpdate.js
+++ src/events/ancientluna/guildMemberUpdate.js
@@ -8,9 +8,7 @@ module.exports = {
      * @param {import("discord.js").GuildMember} newMember
      */
     async execute(client, oldMember, newMember) {
-        // Only check if roles have changed
         if (oldMember.roles.cache.size !== newMember.roles.cache.size) {
-            // Ensure we are in the correct server
             if (newMember.guild.id !== client.config.ancientLunaServer) return;
             await syncMemberRoles(newMember);
         }
