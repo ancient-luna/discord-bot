c5fa674e2dc89a2b7b8dda7de5c58f4a0a923af6
imsoondae <im.daeva@gmail.com>
2025-11-28 06:01:21 +0700

    saviour of the listeners command

--- src/scripts/Message.js
+++ src/scripts/Message.js
@@ -5,22 +5,17 @@ const { join } = require('path');
  */
 module.exports = (client) => {
     let count = 0;
-
     const loadCommands = (dir) => {
         const files = readdirSync(dir);
-        
         for (const file of files) {
             const path = join(dir, file);
             const stat = statSync(path);
-
             if (stat.isDirectory()) {
                 loadCommands(path);
             } else if (file.endsWith('.js') || file.endsWith('.mjs')) {
                 const command = require(path);
                 const parentDir = dir.split(/[\\/]/).pop();
-
                 if (command.category && command.category !== parentDir) command.category = parentDir;
-                
                 client.Commands.set(command.name, command);
                 if (command.aliases && Array.isArray(command.aliases)) {
                     for (const alias of command.aliases) client.Aliases.set(alias, command.name);
@@ -29,7 +24,6 @@ module.exports = (client) => {
             }
         }
     };
-
     loadCommands(join(__dirname, "..", "commands", "message"));
     client.console.log(`Loaded: ${count}`, "cmd");
 }
