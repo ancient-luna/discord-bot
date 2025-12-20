b087b3b8bdf1b0b21e065392392e2ce1fb1e138c
imsoondae <im.daeva@gmail.com>
2025-12-20 19:31:30 +0700

    A Relic: Famed

--- src/events/ancientluna/guildMemberRemove.js
+++ src/events/ancientluna/guildMemberRemove.js
@@ -1,4 +1,4 @@
-const { EmbedBuilder } = require("discord.js");
+const { EmbedBuilder, ContainerBuilder, TextDisplayBuilder, MessageFlags } = require("discord.js");
 
 module.exports = new Object({
     name: "guildMemberRemove",
@@ -7,12 +7,12 @@ module.exports = new Object({
         if (member.partial) await member.user.fetch();
         if (!member.guild) return;
         const channel = member.guild.channels.cache.get(client.config.gatewayChannel);
-        if (!channel) return console.log(`❎ ${member.user.globalName} [${member.user.id}] left from ${member.guild.name} [${member.guild.id}]`);
-        const leavingText = new EmbedBuilder()
-            .setDescription(`The lights get dimmed! **${member.displayName}** leaving the sanctuary`)
-            .setFooter({ text: `${member.user.id} (u) ${member.user.username}`, iconURL: member.user.displayAvatarURL({ dynamic: true, size: 512 }) })
-            .setColor('Red')
-            .setTimestamp();
-        return channel.send({ embeds: [leavingText] })
+        if (!channel) return console.log(`[x] ID: ${member.user.id} left from [${member.guild.id}] ${member.guild.name}`);
+        const memberAvatar = member.user.displayAvatarURL({ dynamic: true, size: 4096 });
+        const container = new ContainerBuilder()
+        const textLeave = new TextDisplayBuilder().setContent(`<:ico_leave:1451833368236003378> **${member.displayName}** leaving the sanctuary`);
+        const textAccount = new TextDisplayBuilder().setContent(`-# ID: ${member.user.id} [(relic)](${memberAvatar}) ${member.user.username}`);
+        container.addTextDisplayComponents(textLeave);
+        return channel.send({ flags: MessageFlags.IsComponentsV2, components: [container] })
     }
 })
