7308e5d41505914189082b856f6903ba67fecc56
imsoondae <daesoon.me@gmail.com>
2026-01-14 22:14:02 +0700

    just logs

--- src/events/ancientluna/guildMemberRemove.js
+++ src/events/ancientluna/guildMemberRemove.js
@@ -6,13 +6,14 @@ module.exports = new Object({
     async execute(client, member) {
         if (member.partial) await member.user.fetch();
         if (!member.guild) return;
-        const channel = member.guild.channels.cache.get(client.config.gatewayChannel);
-        if (!channel) return console.log(`[x] ID: ${member.user.id} left from [${member.guild.id}] ${member.guild.name}`);
-        const memberAvatar = member.user.displayAvatarURL({ dynamic: true, size: 4096 });
-        const container = new ContainerBuilder()
-        const textLeave = new TextDisplayBuilder().setContent(`<:ico_leave:1451833368236003378> **${member.displayName}** leaving the sanctuary`);
-        const textAccount = new TextDisplayBuilder().setContent(`-# ID: ${member.user.id} [(relic)](${memberAvatar}) ${member.user.username}`);
-        container.addTextDisplayComponents(textLeave, textAccount);
-        return channel.send({ flags: MessageFlags.IsComponentsV2, components: [container] })
+        // const channel = member.guild.channels.cache.get(client.config.gatewayChannel);
+        // if (!channel) return console.log(`[x] ID: ${member.user.id} left from [${member.guild.id}] ${member.guild.name}`);
+        return console.log(`[x] ID: ${member.user.id} left from [${member.guild.id}] ${member.guild.name}`);
+        // const memberAvatar = member.user.displayAvatarURL({ dynamic: true, size: 4096 });
+        // const container = new ContainerBuilder()
+        // const textLeave = new TextDisplayBuilder().setContent(`<:ico_leave:1451833368236003378> **${member.displayName}** leaving the sanctuary`);
+        // const textAccount = new TextDisplayBuilder().setContent(`-# ID: ${member.user.id} [(relic)](${memberAvatar}) ${member.user.username}`);
+        // container.addTextDisplayComponents(textLeave, textAccount);
+        // return channel.send({ flags: MessageFlags.IsComponentsV2, components: [container] })
     }
 })
