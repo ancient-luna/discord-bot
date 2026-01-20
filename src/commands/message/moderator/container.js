91672415663e9e7ee13437ccf739db0c18fe4346
imsoondae <daesoon.me@gmail.com>
2026-01-20 08:54:07 +0700

    #1again

--- src/commands/message/moderator/container.js
+++ src/commands/message/moderator/container.js
@@ -18,11 +18,11 @@ module.exports = new Object({
     player: { voice: false, active: false, dj: false, },
 
     async execute(client, message, args) {
-        const text = message.content.split(' ').slice(1).join(' ');
-        if (!text) return message.reply("You.. **you** need to write something for me,\n-# not even Ancestor can save you from this");
+        // const text = message.content.split(' ').slice(1).join(' ');
+        // if (!text) return message.reply("You.. **you** need to write something for me,\n-# not even Ancestor can save you from this");
 
         const container = new ContainerBuilder()
-        const textDisplay = new TextDisplayBuilder().setContent(text)
+        const textDisplay = new TextDisplayBuilder().setContent('Questions or concerns about the LUNA+ community? Use Open Ticket for **server-related support only** (roles, access, features, rules, or technical issues). Your message will be seen only by the Lunarias, and handled quietly beneath the moon.')
         const separator = new SeparatorBuilder({ spacing: SeparatorSpacingSize.Large })
         const textFooter = new TextDisplayBuilder().setContent(`-# *The Commission Request option is currently unavailable, as the commission appeal system is still being prepared and will open soon.`)
         const button = new ActionRowBuilder().addComponents(
@@ -45,11 +45,11 @@ module.exports = new Object({
                 .setStyle(ButtonStyle.Secondary)
                 .setLabel(`See Your Fellow Card`)
                 .setEmoji({
-                    name: 'ancient_luna_plus',
-                    id: '1462981888883949680'
+                    name: 'luna_cool_glasses',
+                    id: '1461723408441151806'
                 })
             )
-        const textDisplays = new TextDisplayBuilder().setContent(`${text} <:ico_radiance:1334864373331787827>`)
+        const textDisplays = new TextDisplayBuilder().setContent(`Here, you can **generate your unique LUNA+ member card**, learn how to set the LUNA+ **server tag** on your profile across desktop and mobile, and unlock the special <@&1459924462744113243> role, automatically bestowed by <@839380589508558910> upon those who wear the tag. Choose a path below and let your presence shine among your fellow cards <:ico_radiance:1334864373331787827>`)
         const buttons = new ActionRowBuilder().addComponents(
             new ButtonBuilder()
                 .setCustomId('btn-tagguildpc')
@@ -69,15 +69,15 @@ module.exports = new Object({
                 })
         )
 
-        // container.addTextDisplayComponents(textDisplay)
-        // container.addSeparatorComponents(separator)
-        // container.addTextDisplayComponents(textFooter)
-        // container.addActionRowComponents(button)
-
-        container.addSectionComponents(section)
+        container.addTextDisplayComponents(textDisplay)
         container.addSeparatorComponents(separator)
-        container.addTextDisplayComponents(textDisplays)
-        container.addActionRowComponents(buttons)
+        container.addTextDisplayComponents(textFooter)
+        container.addActionRowComponents(button)
+
+        // container.addSectionComponents(section)
+        // container.addSeparatorComponents(separator)
+        // container.addTextDisplayComponents(textDisplays)
+        // container.addActionRowComponents(buttons)
 
         const components = [container]
         const attachment = message.attachments.first()
