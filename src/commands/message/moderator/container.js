ae90e6c4966e09495cfd79435d7b72f9b9d28314
imsoondae <daesoon.me@gmail.com>
2026-01-21 15:06:29 +0700

    walk tall

--- src/commands/message/moderator/container.js
+++ src/commands/message/moderator/container.js
@@ -87,15 +87,15 @@ module.exports = new Object({
                 }),
         )
 
-        container.addTextDisplayComponents(textDisplay)
-        container.addSeparatorComponents(separator)
-        container.addTextDisplayComponents(textFooter)
-        container.addActionRowComponents(button)
-
-        // container.addSectionComponents(section)
+        // container.addTextDisplayComponents(textDisplay)
         // container.addSeparatorComponents(separator)
-        // container.addTextDisplayComponents(textDisplays)
-        // container.addActionRowComponents(buttons)
+        // container.addTextDisplayComponents(textFooter)
+        // container.addActionRowComponents(button)
+
+        container.addSectionComponents(section)
+        container.addSeparatorComponents(separator)
+        container.addTextDisplayComponents(textDisplays)
+        container.addActionRowComponents(buttons)
 
         // const components = [container]
         // const attachment = message.attachments.first()
@@ -104,6 +104,6 @@ module.exports = new Object({
         //     components.unshift(mediaGallery)
         // }
 
-        return message.channel.send({ flags: MessageFlags.IsComponentsV2, components: [media, container], allowedMentions: { parse: [] } })
+        return message.channel.send({ flags: MessageFlags.IsComponentsV2, components: [medias, container], allowedMentions: { parse: [] } })
     }
 });
