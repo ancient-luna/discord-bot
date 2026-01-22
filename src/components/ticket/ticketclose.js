3fac7cc894d7e662644bc37672a08bf36f10e7f2
imsoondae <daesoon.me@gmail.com>
2026-01-23 03:05:04 +0700

    say it works. in good

--- src/components/ticket/ticketclose.js
+++ src/components/ticket/ticketclose.js
@@ -1,6 +1,5 @@
-const { PermissionsBitField, MessageFlags, ContainerBuilder, TextDisplayBuilder, FileBuilder, AttachmentBuilder } = require("discord.js");
+const { PermissionsBitField, MessageFlags, ContainerBuilder, TextDisplayBuilder, FileBuilder } = require("discord.js");
 const discordTranscripts = require('discord-html-transcripts');
-const puppeteer = require('puppeteer');
 
 module.exports = {
   name: "ticketclose",
@@ -24,58 +23,16 @@ module.exports = {
     await interaction.deferReply({ flags: MessageFlags.Ephemeral });
 
     try {
-      const htmlTranscript = await discordTranscripts.createTranscript(channel, {
+      const attachment = await discordTranscripts.createTranscript(channel, {
+        filename: `${channelName}-transcript.html`,
         footerText: `Exported {number} message{s}.`,
         poweredBy: false,
-        returnType: 'string'
+        saveImages: true
       });
-
-      const browser = await puppeteer.launch({
-        headless: true,
-        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || null,
-        args: [
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-          '--disable-gpu',
-          '--disable-dev-shm-usage'
-        ]
-      });
-      const page = await browser.newPage();
-
-      await page.setViewport({ width: 1280, height: 800 });
-      await page.setContent(htmlTranscript, { waitUntil: 'networkidle0' });
-
-      const screenshot = await page.screenshot({ fullPage: true });
-
-      const dimensions = await page.evaluate(() => {
-        return {
-          width: document.documentElement.scrollWidth,
-          height: document.documentElement.scrollHeight
-        };
-      });
-
-      const screenshotBase64 = screenshot.toString('base64');
-      await page.setContent(`
-        <body style="margin: 0; padding: 0;">
-            <img src="data:image/png;base64,${screenshotBase64}" style="width: 100%; display: block;">
-        </body>
-    `);
-
-      const pdfResult = await page.pdf({
-        width: `${dimensions.width}px`,
-        height: `${dimensions.height}px`,
-        printBackground: true,
-        pageRanges: '1'
-      });
-
-      await browser.close();
-
-      const pdfBuffer = Buffer.from(pdfResult);
-      const attachment = new AttachmentBuilder(pdfBuffer, { name: `${interaction.channel.name}-transcript.pdf` });
 
       const container = new ContainerBuilder()
       const text = new TextDisplayBuilder().setContent(`-# <:srv_attachment:1334881013943504980> **#${channelName}** closed by ${interaction.user.displayName}`)
-      const file = new FileBuilder().setURL(`attachment://${interaction.channel.name}-transcript.pdf`)
+      const file = new FileBuilder().setURL(`attachment://${channelName}-transcript.html`)
 
       container.addTextDisplayComponents(text)
       container.addFileComponents(file)
