1a11ac4f4df674dbf3245ddc40e70b0b862df2a0
imsoondae <daesoon.me@gmail.com>
2026-01-27 16:30:47 +0700

    the wolf howls but the hawk tuahs

--- src/commands/message/fun/spray.js
+++ src/commands/message/fun/spray.js
@@ -11,12 +11,12 @@ module.exports = new Object({
     args: false,
     permissions: { client: [], user: [], dev: false, },
     player: { voice: false, active: false, dj: false, },
-    
+
     async execute(client, message, args) {
         let loadingTxt = await message.channel.send(`<a:mm_spray:1369204415298408448> *spraying o wo' ooh bo' ooh ...*`);
         let SRCtomato = new AttachmentBuilder("src/assets/react/tomatooo.mp4")
         await loadingTxt.edit({
-            content: "<@396683781119148032> threw facts and dealt ...",
+            content: "the ~~wolf~~ peanut howls but the hawk tuahs...",
             files: [SRCtomato]
         });
         await message.channel.send('# 𝒆𝒎𝒐𝒕𝒊𝒐𝒏𝒂𝒍 𝒅𝒂𝒎𝒂𝒈𝒆').then(msg => {
