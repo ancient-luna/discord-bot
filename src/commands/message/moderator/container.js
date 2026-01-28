c39555af008758c7550ab3f4d2d8f038f25cc58b
imsoondae <daesoon.me@gmail.com>
2026-01-28 21:16:26 +0700

    actual c#1

--- src/commands/message/moderator/container.js
+++ src/commands/message/moderator/container.js
@@ -82,15 +82,15 @@ module.exports = new Object({
                 }),
         )
 
-        // container.addTextDisplayComponents(textDisplay)
+        container.addTextDisplayComponents(textDisplay)
         // // container.addSeparatorComponents(separator)
-        // container.addTextDisplayComponents(textFooter)
-        // container.addActionRowComponents(button)
+        container.addTextDisplayComponents(textFooter)
+        container.addActionRowComponents(button)
 
-        container.addSectionComponents(section)
-        container.addSeparatorComponents(separator)
-        container.addTextDisplayComponents(textDisplays)
-        container.addActionRowComponents(buttons)
+        // container.addSectionComponents(section)
+        // container.addSeparatorComponents(separator)
+        // container.addTextDisplayComponents(textDisplays)
+        // container.addActionRowComponents(buttons)
 
         // const components = [container]
         // const attachment = message.attachments.first()
@@ -99,6 +99,6 @@ module.exports = new Object({
         //     components.unshift(mediaGallery)
         // }
 
-        return message.channel.send({ flags: MessageFlags.IsComponentsV2, components: [medias, container], allowedMentions: { parse: [] } })
+        return message.channel.send({ flags: MessageFlags.IsComponentsV2, components: [media, container], allowedMentions: { parse: [] } })
     }
 });
