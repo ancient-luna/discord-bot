4c66a3dc2408f2004acb4b682c6c98a695397cfa
imsoondae <daesoon.me@gmail.com>
2026-01-28 21:12:25 +0700

    c1

--- src/commands/message/moderator/container.js
+++ src/commands/message/moderator/container.js
@@ -29,17 +29,12 @@ module.exports = new Object({
             }])
         const textDisplay = new TextDisplayBuilder().setContent('Questions or concerns about the LUNA+ community? Use Open Ticket for **server-related support only** (roles, access, features, rules, or technical issues). Your message will be seen only by the Lunarias, and handled quietly beneath the moon.')
         const separator = new SeparatorBuilder({ spacing: SeparatorSpacingSize.Large })
-        const textFooter = new TextDisplayBuilder().setContent(`-# *The Commission Request option is currently unavailable, as the commission appeal system is still being prepared and will open soon.`)
+        const textFooter = new TextDisplayBuilder().setContent(`-# Includes the \`open commission\` tag appeal for <#1462415912916353241>`)
         const button = new ActionRowBuilder().addComponents(
             new ButtonBuilder()
                 .setCustomId('btn-ticketopen')
-                .setLabel('Open Ticket')
+                .setLabel('Create a Support Ticket')
                 .setStyle(ButtonStyle.Primary),
-            new ButtonBuilder()
-                .setCustomId('btn-opencommission')
-                .setLabel('Commission Permission Request')
-                .setStyle(ButtonStyle.Secondary)
-                .setDisabled(true),
         )
 
         const medias = new MediaGalleryBuilder()
@@ -62,22 +57,6 @@ module.exports = new Object({
         const textDisplays = new TextDisplayBuilder().setContent(`Here, you can **generate your unique LUNA+ member card**, learn how to set the LUNA+ **server tag** on your profile across desktop and mobile, and unlock the special <@&1459924462744113243> role, automatically bestowed by <@839380589508558910> upon those who wear the tag. Choose a path below and let your presence shine among your fellow cards <:ico_radiance:1334864373331787827>`)
         const buttons = new ActionRowBuilder().addComponents(
             new ButtonBuilder()
-                .setCustomId('btn-tagguildpc')
-                .setLabel('Tag Tutorial (Desktop)')
-                .setStyle(ButtonStyle.Secondary)
-                .setEmoji({
-                    name: 'ic_repost',
-                    id: '1334863701026541648'
-                }),
-            new ButtonBuilder()
-                .setCustomId('btn-tagguildmobile')
-                .setLabel('Tag Tutorial (Mobile)')
-                .setStyle(ButtonStyle.Secondary)
-                .setEmoji({
-                    name: 'ic_repost',
-                    id: '1334863701026541648'
-                }),
-            new ButtonBuilder()
                 .setCustomId('btn-ost')
                 .setLabel('LUNA+ OST')
                 .setStyle(ButtonStyle.Secondary)
@@ -87,15 +66,15 @@ module.exports = new Object({
                 }),
         )
 
-        // container.addTextDisplayComponents(textDisplay)
+        container.addTextDisplayComponents(textDisplay)
         // container.addSeparatorComponents(separator)
-        // container.addTextDisplayComponents(textFooter)
-        // container.addActionRowComponents(button)
+        container.addTextDisplayComponents(textFooter)
+        container.addActionRowComponents(button)
 
-        container.addSectionComponents(section)
-        container.addSeparatorComponents(separator)
-        container.addTextDisplayComponents(textDisplays)
-        container.addActionRowComponents(buttons)
+        // container.addSectionComponents(section)
+        // container.addSeparatorComponents(separator)
+        // container.addTextDisplayComponents(textDisplays)
+        // container.addActionRowComponents(buttons)
 
         // const components = [container]
         // const attachment = message.attachments.first()
@@ -104,6 +83,6 @@ module.exports = new Object({
         //     components.unshift(mediaGallery)
         // }
 
-        return message.channel.send({ flags: MessageFlags.IsComponentsV2, components: [medias, container], allowedMentions: { parse: [] } })
+        return message.channel.send({ flags: MessageFlags.IsComponentsV2, components: [media, container], allowedMentions: { parse: [] } })
     }
 });
