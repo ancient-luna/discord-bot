bd3fb3df6f9476673934c4d9817095e0e82b41ca
imsoondae <daesoon.me@gmail.com>
2026-02-05 11:07:52 +0700

    Celestia Luna+

--- src/events/ancientluna/guildMemberTag.js
+++ src/events/ancientluna/guildMemberTag.js
@@ -6,15 +6,12 @@ module.exports = {
     async execute(client, oldUser, newUser) {
         const config = client.config;
         const guildsToSync = [config.lunaServer, config.celestuneServer].filter(id => id);
-
         try {
             const userProfile = await newUser.fetch(true).catch(() => null);
             if (!userProfile) return;
-
             for (const guildId of guildsToSync) {
                 const guild = client.guilds.cache.get(guildId);
                 if (!guild) continue;
-
                 const member = await guild.members.fetch(newUser.id).catch(() => null);
                 if (member) {
                     await syncMemberTagRoles(member, userProfile);
